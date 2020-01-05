3c97f8fccda402d88ff85ba26c099d9f3e1a4c12
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-01-05 08:47:26 +0000

    2020-01-05

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- stopka -->
 <footer>
-    <p class="text-left tiny">Aktualizacja: 29.&nbsp;grudnia 2019</p>
+    <p class="text-left tiny">Aktualizacja: 5.&nbsp;stycznia 2020&nbsp;r.</p>
     <p>Parafia Podwyższenia Krzyża Świętego &copy;
        2012-<script>document.write(new Date().getFullYear());</script>
        Wszelkie prawa zastrzeżone.<br />
