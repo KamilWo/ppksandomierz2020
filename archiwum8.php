8b062a4991defd77fbdc84a34bde6bfc1dd8958e
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-01-05 08:11:59 +0000

    2020-01-05

--- archiwum8.php
+++ archiwum8.php
@@ -13,7 +13,120 @@ include 'head.php'
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2019 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2019 - 22.&nbsp;grudnia 2019 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2019 - 31.&nbsp;grudnia 2019 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>29.&nbsp;grudnia 2019&nbsp;r.</h3>
+        <h3>Niedziela w&nbsp;Oktawie Narodzenia Pańskiego</h3>
+        <h3>Święto Świętej Rodziny: Jezusa, Maryi i&nbsp;Józefa</h3>
+    </div>
+    <div class="col">
+        <p>1. We&nbsp;wtorek o&nbsp;godzinie 17.00 odprawimy Nieszpory
+            ze&nbsp;śpiewem Święty Boże i&nbsp;Ciebie Boga wysławiamy.
+            Zapraszamy wszystkich, a&nbsp;szczególnie rodziny, które
+            przeżywały w&nbsp;tym roku w&nbsp;rodzinie chrzest dziecka,
+            I&nbsp;Komunię Świętą, bierzmowanie, zaślubiny, czy odejście
+            do&nbsp;wieczności bliskich osób.</p>
+        <p>2. W&nbsp;środę 1&nbsp;stycznia, Nowy Rok, uroczystość Świętej
+            Bożej Rodzicielki. Msze Święte jak w&nbsp;niedziele, także
+            o&nbsp;19.30.</p>
+        <p>3. W&nbsp;tym tygodniu jest I&nbsp;czwartek, piątek i&nbsp;sobota.
+            Spowiedź w&nbsp;piątek od&nbsp;17.30. Nie będzie odwiedzin
+            chorych, z&nbsp;racji wizyty duszpasterskiej.</p>
+        <p>4. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę na&nbsp;godzinę
+                7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 8 od&nbsp;11
+            do&nbsp;20 włącznie.</p>
+        <p>5. Zmarł Marcin Ozdoba, jego pogrzeb będzie jutro
+            o&nbsp;godz.&nbsp;12.00. <em>Wieczny odpoczynek...</em></p>
+        <p>6. W&nbsp;dniach 3-12&nbsp;lipca 2020 roku organizowana jest
+            pielgrzymko-wycieczka do&nbsp;Medjugorie oraz innych miejsc
+            w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech. Bliższe
+            informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
+            do&nbsp;kościoła.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 29.&nbsp;grudnia 2019&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br/>
+
+<div class="card">
+    <div class="card-header">
+        <h3>Wizyta duszpasterska:</h3>
+    </div>
+    <div class="col">
+        <p><strong>29.12. - dzisiaj, od&nbsp;godz.&nbsp;14.30:</strong><br />
+            3&nbsp;ks.&nbsp;ul.&nbsp;Czachowskiego 3, 5, 7;<br />
+            <strong>ok.&nbsp;godz.&nbsp;16.00</strong> ul.&nbsp;Czachowskiego
+            9, 11, 13;<br />
+            <strong>ok.&nbsp;godz.&nbsp;18.00</strong> ul.&nbsp;Czachowskiego
+            12, 14, 17.</p>
+        <p><strong>30.12. - jutro, od&nbsp;godz.&nbsp;9.00:</strong><br />
+            2&nbsp;ks.&nbsp;Milczany wieś;<br />
+            2&nbsp;ks.&nbsp;Milczany i&nbsp;Kobierniki po&nbsp;szosie
+            - od&nbsp;myjni,<br />
+            1&nbsp;ks. ul.&nbsp;Mickiewicza domy prywatne i&nbsp;domy
+            po&nbsp;mieście wraz z&nbsp;szeregówką przy
+            ul.&nbsp;Maciejowskiego;<br />
+            <strong>od&nbsp;godz.&nbsp;14.30</strong> 1&nbsp;ks.&nbsp;blok
+            ul.&nbsp;Maciejowskiego 4.</p>
+        <p><strong>31.12. - w&nbsp;Sylwestra, we&nbsp;wtorek,
+            od&nbsp;godz.&nbsp;9.00:</strong><br />
+            2&nbsp;ks.&nbsp;Kobierniki wieś,<br />
+            2&nbsp;ks.&nbsp;Kobierniki Górne,<br />
+            2&nbsp;ks.&nbsp;ul.&nbsp;Polna.</p>
+        <p><strong>01.01.2020&nbsp;r. - w&nbsp;Nowy Rok,
+            od&nbsp;godz.&nbsp;14.30:</strong><br />
+            bloki ul.&nbsp;Cieśli 1, 3, 7.</p>
+        <p><strong>02.01. - od&nbsp;godz.&nbsp;9.00:</strong><br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;C.&nbsp;Skłodowskiej
+            i&nbsp;Młodożeńca,<br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Dąbrowskiego,<br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Wąska, Różana, Szczęśliwa,<br />
+            <strong>od&nbsp;godz.&nbsp;14.30</strong> 1&nbsp;ks.&nbsp;blok
+            ul.&nbsp;Maciejowskiego 7.</p>
+        <p><strong>03.01. - od&nbsp;godz.&nbsp;9.00:</strong><br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Harcerska i&nbsp;Batalionów
+            Chłopskich,<br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Morelowa i&nbsp;Wiśniowa,<br />
+            <strong>od&nbsp;godz.&nbsp;13.30</strong>
+            1&nbsp;ks.&nbsp;ul.&nbsp;Ożarowska przy kościele
+            do&nbsp;świateł,<br />
+            <strong>od&nbsp;godz.&nbsp;14.30</strong> 1&nbsp;ks.&nbsp;bloki
+            ul.&nbsp;Mickiewicza 57, później 59,<br />
+            <strong>od&nbsp;godz.&nbsp;18.30</strong>
+            2&nbsp;ks.&nbsp;bloki ul.&nbsp;Maciejowskiego 32 i&nbsp;34.</p>
+        <p><strong>04.01. - od&nbsp;godz.&nbsp;9.00:</strong><br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Kochanowskiego
+            i&nbsp;Wiosenna,<br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Westerplatte, Przytulna
+            i&nbsp;Kwiatowa,<br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Wiejska, Frankowskiego, Reja,<br />
+            <strong>od&nbsp;14.30</strong> 1&nbsp;ks.&nbsp;blok
+            ul.&nbsp;Mickiewicza 49,<br />
+            <strong>od&nbsp;godz.&nbsp;17.00</strong>
+            2&nbsp;ks.&nbsp;ul.&nbsp;Mickiewicza 47 i&nbsp;55.</p>
+        <p><strong>05.01. - Niedziela, od godz. 14.30:</strong><br />
+            2&nbsp;ks.&nbsp;ul.&nbsp;Maciejowskiego 11,<br />
+            1&nbsp;ks.&nbsp;ul.&nbsp;Maciejowskiego 10,<br />
+            <strong>od&nbsp;godz.&nbsp;18.30</strong>
+            3&nbsp;ks.&nbsp;ul.&nbsp;Cieśli 9.</p>
+        <p><strong>06.01. - Trzech Króli, od&nbsp;godz.&nbsp;14.30:</strong><br />
+            2&nbsp;ks.&nbsp;ul.&nbsp;Maciejowskiego 40<br />
+            i&nbsp;1&nbsp;ks.&nbsp;Żółkiewskiego 8,<br />
+            <strong>od&nbsp;godz.&nbsp;18.00</strong> 3&nbsp;ks.&nbsp;bloki
+            ul.&nbsp;Czachowskiego 19&nbsp;i&nbsp;Maciejowskiego 38.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 29.&nbsp;grudnia 2019&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>22.&nbsp;grudnia 2019&nbsp;r.</h3>
