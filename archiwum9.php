f4eba88b1d7c3b73b3d7771df10acdc74eea8993
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-01-26 09:28:59 +0000

    2020-01-26

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,58 @@ include 'head.php'
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 12.&nbsp;stycznia 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 19.&nbsp;stycznia 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>19.&nbsp;stycznia 2020&nbsp;r.</h3>
+        <h3>II Niedziela Zwykła - Rok A</h3>
+    </div>
+    <div class="col">
+        <p>1. Kandydatów do&nbsp;sakramentu bierzmowania z&nbsp;klas
+            8&nbsp;zapraszamy na&nbsp;spotkanie formacyjne we&nbsp;wtorek -
+            21&nbsp;stycznia. Grupa 1&nbsp;godz.&nbsp;17.00, grupa
+            2&nbsp;godzina 18.30.</p>
+        <p>2. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
+            Kazimierza Tenderendę i&nbsp;Zdzisława Bernasia. Polećmy ich
+            Bożemu Miłosierdziu odmawiając: <em>Wieczny odpoczynek...</em></p>
+        <p>3. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę na&nbsp;godzinę
+                7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 7, numery
+            od&nbsp;1 do&nbsp;10 włącznie.</p>
+        <p>4. Na&nbsp;stoliku pod chórem czasopisma Niedziela i&nbsp;Gość
+            Niedzielny – cena po&nbsp;6&nbsp;złotych.</p>
+        <p>5. W&nbsp;piątek 24&nbsp;stycznia o&nbsp;godz.&nbsp;18.15
+            w&nbsp;Bazylice Katedralnej w&nbsp;Sandomierzu podczas Tygodnia
+            Modlitw o&nbsp;Jedność Chrześcijan odbędzie się koncert
+            Kameralnego Chóru Męskiego kościoła prawosławnego parafii
+            św.&nbsp;Jana Klimaka z&nbsp;Warszawy. Zapraszamy serdecznie
+            do&nbsp;wysłuchania koncertu oraz modlitwy o&nbsp;jedność
+            Chrześcijan.</p>
+        <p>6. W&nbsp;sobotę 25&nbsp;stycznia w&nbsp;Wyższym Seminarium
+            Duchownym uroczystość ku&nbsp;czci świętego Tomasza
+            z&nbsp;Akwinu. Msza Święta koncelebrowana pod przewodnictwem
+            Księdza Biskupa Krzysztofa Nitkiewicza w&nbsp;kościele
+            seminaryjnym o&nbsp;godz.&nbsp;10.00. Serdecznie zapraszamy.</p>
+        <p>7. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
+            Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
+            przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>8. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+            jak również ludziom starszym, schorowanym, którzy już osobiście
+            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+            we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
+            i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
+            intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 19.&nbsp;stycznia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>12.&nbsp;stycznia 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,40 +1,30 @@
 <div class="card">
     <div class="card-header">
-        <h3>19.&nbsp;stycznia 2020&nbsp;r.</h3>
-        <h3>II Niedziela Zwykła - Rok A</h3>
+        <h3>26.&nbsp;stycznia 2020&nbsp;r.</h3>
+        <h3>III Niedziela Zwykła - Rok A</h3>
     </div>
     <div class="col">
-        <p>1. Kandydatów do&nbsp;sakramentu bierzmowania z&nbsp;klas
-            8&nbsp;zapraszamy na&nbsp;spotkanie formacyjne we&nbsp;wtorek -
-            21&nbsp;stycznia. Grupa 1&nbsp;godz.&nbsp;17.00, grupa
-            2&nbsp;godzina 18.30.</p>
-        <p>2. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
-            Kazimierza Tenderendę i&nbsp;Zdzisława Bernasia. Polećmy ich
-            Bożemu Miłosierdziu odmawiając: <em>Wieczny odpoczynek...</em></p>
+        <p>1. W&nbsp;tym tygodniu przypada pierwsza sobota miesiąca.
+            Wystawienie Najświętszego Sakramentu i&nbsp;nabożeństwo
+            pierwszosobotnie rozpocznie się o&nbsp;godz.&nbsp;17.15.</p>
+        <p>2. W&nbsp;przyszłą niedzielę będziemy obchodzić Święto Ofiarowania
+            Pańskiego. Jest to&nbsp;dzień modlitw w&nbsp;intencji osób życia
+            konsekrowanego.</p>
         <p>3. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
             ofiarę, dary stołu i&nbsp;wszelką życzliwość.<br />
             <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę na&nbsp;godzinę
             7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 7, numery
-            od&nbsp;1 do&nbsp;10 włącznie.</p>
-        <p>4. Na&nbsp;stoliku pod chórem czasopisma Niedziela i&nbsp;Gość
-            Niedzielny – cena po&nbsp;6&nbsp;złotych.</p>
-        <p>5. W&nbsp;piątek 24&nbsp;stycznia o&nbsp;godz.&nbsp;18.15
-            w&nbsp;Bazylice Katedralnej w&nbsp;Sandomierzu podczas Tygodnia
-            Modlitw o&nbsp;Jedność Chrześcijan odbędzie się koncert
-            Kameralnego Chóru Męskiego kościoła prawosławnego parafii
-            św.&nbsp;Jana Klimaka z&nbsp;Warszawy. Zapraszamy serdecznie
-            do&nbsp;wysłuchania koncertu oraz modlitwy o&nbsp;jedność
-            Chrześcijan.</p>
-        <p>6. W&nbsp;sobotę 25&nbsp;stycznia w&nbsp;Wyższym Seminarium
-            Duchownym uroczystość ku&nbsp;czci świętego Tomasza
-            z&nbsp;Akwinu. Msza Święta koncelebrowana pod przewodnictwem
-            Księdza Biskupa Krzysztofa Nitkiewicza w&nbsp;kościele
-            seminaryjnym o&nbsp;godz.&nbsp;10.00. Serdecznie zapraszamy.</p>
-        <p>7. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            od&nbsp;11 do&nbsp;20 włącznie. <em>Bóg zapłać</em> panu
+            Tadeuszowi i&nbsp;Ryszardowi za&nbsp;pracę na&nbsp;rzecz
+            parafii.</p>
+        <p>4. Na&nbsp;stoliku pod chórem są&nbsp;czasopisma Niedziela
+            i&nbsp;Gość Niedzielny. Zachęcamy do&nbsp;nabywania
+            i&nbsp;czytania.</p>
+        <p>5. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
             innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
             Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
             przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>8. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>6. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
             urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
             jak również ludziom starszym, schorowanym, którzy już osobiście
             nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
@@ -42,7 +32,7 @@
             i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
             intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 19.&nbsp;stycznia 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 26.&nbsp;stycznia 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
