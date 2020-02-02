0b812a11c1605f4402314751a6ceb5f426d1d5df
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-02-02 08:57:36 +0000

    2020-02-02

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,49 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 19.&nbsp;stycznia 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 26.&nbsp;stycznia 2020 r.</h2>
+
+<div class="card">
+    <div class="card-header">
+        <h3>26.&nbsp;stycznia 2020&nbsp;r.</h3>
+        <h3>III Niedziela Zwykła - Rok A</h3>
+    </div>
+    <div class="col">
+        <p>1. W&nbsp;tym tygodniu przypada pierwsza sobota miesiąca.
+            Wystawienie Najświętszego Sakramentu i&nbsp;nabożeństwo
+            pierwszosobotnie rozpocznie się o&nbsp;godz.&nbsp;17.15.</p>
+        <p>2. W&nbsp;przyszłą niedzielę będziemy obchodzić Święto Ofiarowania
+            Pańskiego. Jest to&nbsp;dzień modlitw w&nbsp;intencji osób życia
+            konsekrowanego.</p>
+        <p>3. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę na&nbsp;godzinę
+                7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 7, numery
+            od&nbsp;11 do&nbsp;20 włącznie. <em>Bóg zapłać</em> panu
+            Tadeuszowi i&nbsp;Ryszardowi za&nbsp;pracę na&nbsp;rzecz
+            parafii.</p>
+        <p>4. Na&nbsp;stoliku pod chórem są&nbsp;czasopisma Niedziela
+            i&nbsp;Gość Niedzielny. Zachęcamy do&nbsp;nabywania
+            i&nbsp;czytania.</p>
+        <p>5. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
+            Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
+            przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>6. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+            jak również ludziom starszym, schorowanym, którzy już osobiście
+            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+            we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
+            i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
+            intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 26.&nbsp;stycznia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>19.&nbsp;stycznia 2020&nbsp;r.</h3>
