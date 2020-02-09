1f31b03df6f70a6f89ba77d39ffca198145c75cc
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-02-09 08:57:42 +0000

    2020-02-09

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,63 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 26.&nbsp;stycznia 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 2.&nbsp;lutego 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>2.&nbsp;lutego 2020&nbsp;r.</h3>
+        <h3>Niedziela - Rok A</h3>
+        <h3>Święto Ofiarowania Pańskiego</h3>
+    </div>
+    <div class="col">
+        <p>1. W&nbsp;dzisiejszą niedzielę obchodzimy Święto Ofiarowania
+            Pańskiego. Jest to&nbsp;dzień modlitw w&nbsp;intencji osób życia
+            konsekrowanego. Błogosławieństwo świec na&nbsp;każdej
+            Mszy&nbsp;św.</p>
+        <p>2. Dzisiaj o&nbsp;godz.&nbsp;15.30 będzie wystawienie
+            Najświętszego Sakramentu i&nbsp;nabożeństwo różańcowe.
+            Po&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;16.00 zmiana tajemnic
+            różańcowych.</p>
+        <p>3. Jutro na&nbsp;godz.&nbsp;9.00 zapraszamy chętnych
+            do&nbsp;pomocy przy rozbieraniu dekoracji bożonarodzeniowej.</p>
+        <p>4. W&nbsp;tym tygodniu przypada pierwszy czwartek i&nbsp;piątek
+            miesiąca. Spowiedź w&nbsp;pierwszy piątek
+            o&nbsp;godz.&nbsp;17.30. Odwiedzimy chorych z&nbsp;komunia
+            świętą.</p>
+        <p>5. W&nbsp;przyszłą niedzielę zbiórka do&nbsp;puszek
+            na&nbsp;Fundusz Pomocy Chorym Kapłanom Diecezji
+            Sandomierskiej.</p>
+        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
+            z&nbsp;naszej parafii Umberto Romanelli, Mariusza Lipczyńskiego
+            i&nbsp;Zofię Guściora. Polećmy ich Miłosierdziu Bożemu
+            odmawiając: <em>Wieczny odpoczynek...</em></p>
+        <p>7. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę na&nbsp;godzinę
+                7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 7, numery
+            od&nbsp;21 do&nbsp;30 włącznie. <em>Bóg zapłać</em> panu
+            Tadeuszowi i&nbsp;Ryszardowi za&nbsp;pracę na&nbsp;rzecz
+            parafii.</p>
+        <p>8. Na&nbsp;stoliku pod chórem są&nbsp;czasopisma Niedziela
+            i&nbsp;Gość Niedzielny. Zachęcamy do&nbsp;nabywania
+            i&nbsp;czytania. Cena 6&nbsp;PLN.</p>
+        <p>9. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
+            Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
+            przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje
+            imieniny, urodziny, jakiekolwiek rocznice, gościom, uczestnikom
+            liturgii, jak również ludziom starszym, schorowanym, którzy już
+            osobiście nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+            we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
+            i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
+            intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 2.&nbsp;lutego 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
 
 <div class="card">
     <div class="card-header">
--- last_post.php
+++ last_post.php
@@ -1,55 +1,75 @@
 <div class="card">
     <div class="card-header">
-        <h3>2.&nbsp;lutego 2020&nbsp;r.</h3>
-        <h3>Niedziela - Rok A</h3>
-        <h3>Święto Ofiarowania Pańskiego</h3>
+        <h3>9.&nbsp;lutego 2020&nbsp;r.</h3>
+        <h3>V Niedziela - Rok A</h3>
     </div>
     <div class="col">
-        <p>1. W&nbsp;dzisiejszą niedzielę obchodzimy Święto Ofiarowania
-            Pańskiego. Jest to&nbsp;dzień modlitw w&nbsp;intencji osób życia
-            konsekrowanego. Błogosławieństwo świec na&nbsp;każdej
-            Mszy&nbsp;św.</p>
-        <p>2. Dzisiaj o&nbsp;godz.&nbsp;15.30 będzie wystawienie
-            Najświętszego Sakramentu i&nbsp;nabożeństwo różańcowe.
-            Po&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;16.00 zmiana tajemnic
-            różańcowych.</p>
-        <p>3. Jutro na&nbsp;godz.&nbsp;9.00 zapraszamy chętnych
-            do&nbsp;pomocy przy rozbieraniu dekoracji bożonarodzeniowej.</p>
-        <p>4. W&nbsp;tym tygodniu przypada pierwszy czwartek i&nbsp;piątek
-            miesiąca. Spowiedź w&nbsp;pierwszy piątek
-            o&nbsp;godz.&nbsp;17.30. Odwiedzimy chorych z&nbsp;komunia
-            świętą.</p>
-        <p>5. W&nbsp;przyszłą niedzielę zbiórka do&nbsp;puszek
-            na&nbsp;Fundusz Pomocy Chorym Kapłanom Diecezji
-            Sandomierskiej.</p>
-        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
-            z&nbsp;naszej parafii Umberto Romanelli, Mariusza Lipczyńskiego
-            i&nbsp;Zofię Guściora. Polećmy ich Miłosierdziu Bożemu
-            odmawiając: <em>Wieczny odpoczynek...</em></p>
-        <p>7. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
-            ofiarę, dary stołu i&nbsp;wszelką życzliwość.<br />
-            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę na&nbsp;godzinę
-                7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 7, numery
-            od&nbsp;21 do&nbsp;30 włącznie. <em>Bóg zapłać</em> panu
-            Tadeuszowi i&nbsp;Ryszardowi za&nbsp;pracę na&nbsp;rzecz
-            parafii.</p>
-        <p>8. Na&nbsp;stoliku pod chórem są&nbsp;czasopisma Niedziela
-            i&nbsp;Gość Niedzielny. Zachęcamy do&nbsp;nabywania
-            i&nbsp;czytania. Cena 6&nbsp;PLN.</p>
-        <p>9. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        <p>1. W&nbsp;dzisiejszą niedzielę zbiórka do&nbsp;puszek
+            na&nbsp;Fundusz Pomocy Chorym Kapłanom Diecezji Sandomierskiej.</p>
+        <p>2. W&nbsp;poniedziałek zapraszamy rodziców kandydatów
+            do&nbsp;sakramentu bierzmowania z&nbsp;klas VIII
+            na&nbsp;godz.&nbsp;18.00 na&nbsp;Mszę&nbsp;św. i&nbsp;spotkanie
+            w&nbsp;sali parafialnej. Kandydaci do&nbsp;bierzmowania
+            z&nbsp;klas VIII będą mieli swoje spotkanie we&nbsp;wtorek
+            i&nbsp;czwartek o&nbsp;godz.&nbsp;17.00 w&nbsp;sali parafialnej.
+            Bierzmowanie zostało przesunięte na&nbsp;9&nbsp;marca
+            2020&nbsp;r. o&nbsp;godz.&nbsp;18.00.</p>
+        <p>3. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
+            z&nbsp;naszej parafii Monikę Kwiecińską-Gajewską. Jej pogrzeb
+            będzie w&nbsp;środę o&nbsp;godz.&nbsp;14.00. Polećmy
+            ją&nbsp;Miłosierdziu Bożemu odmawiając: <em>Wieczny
+                odpoczynek...</em></p>
+        <p>4. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę (z&nbsp;10 rodzin w&nbsp;klatce włączyły się 2&nbsp;Panie
+            – Basia i&nbsp;Ela), dary stołu i&nbsp;wszelką życzliwość.
+            <em>Bóg zapłać</em> za&nbsp;rozbieranie dekoracji
+            bożonarodzeniowej Pani Monice i&nbsp;Panom: Tadeuszowi,
+            Ryszardowi, Wacławowi, Pawłowi, Wiesławowi, Romanowi, Antoniemu,
+            Marianowi i&nbsp;Stanisławowi oraz Pracownikom Państwa Marii
+            i&nbsp;Bogdana za&nbsp;zabranie, i&nbsp;utylizowanie
+            choinek.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+            na&nbsp;godz.&nbsp;7.00</strong> Rodziny z&nbsp;bloku
+            Maciejowskiego 7 od&nbsp;31 do&nbsp;40.</p>
+        <p>5. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>6. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
             innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
             Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
             przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje
-            imieniny, urodziny, jakiekolwiek rocznice, gościom, uczestnikom
-            liturgii, jak również ludziom starszym, schorowanym, którzy już
-            osobiście nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+        <p>7. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+            jak również ludziom starszym, schorowanym, którzy już osobiście
+            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
             we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
             i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
             intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 2.&nbsp;lutego 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 9.&nbsp;lutego 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
+    </div>
+</div>
+<br />
+
+<div class="card">
+    <div class="card-header">
+        <h3>Ksiądz Dyrektor diecezjalnej Caritas informuje
+            i&nbsp;zaprasza:</h3>
+    </div>
+    <div class="col">
+        <p>We&nbsp;wtorek wspomnienie NMP z&nbsp;Lourdes, XXVIII
+            Światowy Dzień Chorego. Diecezjalne obchody XXVIII
+            Światowego Dnia Chorego pod przewodnictwem Ks.&nbsp;Bpa
+            Ordynariusza Krzysztofa Nitkiewicza odbędą się
+            w&nbsp;Sandomierzu w&nbsp;Kościele Seminaryjnym
+            pw.&nbsp;Św.&nbsp;Michała Archanioła,
+            o&nbsp;godz.&nbsp;10.00 Msza Święta z&nbsp;możliwością
+            przyjęcia sakramentu chorych i&nbsp;błogosławieństwo
+            Najświętszym Sakramentem, o&nbsp;godz.&nbsp;11.30,
+            w&nbsp;Domu Katolickim koncert Pani Halny Frąckowiak
+            zapraszamy osoby chore, niepełnosprawne wraz
+            ze&nbsp;swoimi opiekunami oraz pracowników służby zdrowia
+            i&nbsp;ich duszpasterzy do&nbsp;wspólnej modlitwy.</p>
     </div>
 </div>
