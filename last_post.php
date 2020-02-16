d84c2d626c8632083b7d39cee44b949c0f259ade
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-02-16 08:44:16 +0000

    2020-02-16

--- last_post.php
+++ last_post.php
@@ -1,43 +1,35 @@
 <div class="card">
     <div class="card-header">
-        <h3>9.&nbsp;lutego 2020&nbsp;r.</h3>
-        <h3>V Niedziela - Rok A</h3>
+        <h3>16.&nbsp;lutego 2020&nbsp;r.</h3>
+        <h3>VI Niedziela Zwykła - Rok A</h3>
     </div>
     <div class="col">
-        <p>1. W&nbsp;dzisiejszą niedzielę zbiórka do&nbsp;puszek
-            na&nbsp;Fundusz Pomocy Chorym Kapłanom Diecezji Sandomierskiej.</p>
-        <p>2. W&nbsp;poniedziałek zapraszamy rodziców kandydatów
-            do&nbsp;sakramentu bierzmowania z&nbsp;klas VIII
-            na&nbsp;godz.&nbsp;18.00 na&nbsp;Mszę&nbsp;św. i&nbsp;spotkanie
-            w&nbsp;sali parafialnej. Kandydaci do&nbsp;bierzmowania
-            z&nbsp;klas VIII będą mieli swoje spotkanie we&nbsp;wtorek
-            i&nbsp;czwartek o&nbsp;godz.&nbsp;17.00 w&nbsp;sali parafialnej.
-            Bierzmowanie zostało przesunięte na&nbsp;9&nbsp;marca
-            2020&nbsp;r. o&nbsp;godz.&nbsp;18.00.</p>
-        <p>3. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
-            z&nbsp;naszej parafii Monikę Kwiecińską-Gajewską. Jej pogrzeb
-            będzie w&nbsp;środę o&nbsp;godz.&nbsp;14.00. Polećmy
-            ją&nbsp;Miłosierdziu Bożemu odmawiając: <em>Wieczny
-                odpoczynek...</em></p>
-        <p>4. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
-            ofiarę (z&nbsp;10 rodzin w&nbsp;klatce włączyły się 2&nbsp;Panie
-            – Basia i&nbsp;Ela), dary stołu i&nbsp;wszelką życzliwość.
-            <em>Bóg zapłać</em> za&nbsp;rozbieranie dekoracji
-            bożonarodzeniowej Pani Monice i&nbsp;Panom: Tadeuszowi,
-            Ryszardowi, Wacławowi, Pawłowi, Wiesławowi, Romanowi, Antoniemu,
-            Marianowi i&nbsp;Stanisławowi oraz Pracownikom Państwa Marii
-            i&nbsp;Bogdana za&nbsp;zabranie, i&nbsp;utylizowanie
-            choinek.<br />
+        <p>1. Kochani, kończy się czas karnawału, rozpocznie się czas
+            Wielkiego Postu, przyszła środa to&nbsp;Środa Popielcowa.
+            U&nbsp;nas w&nbsp;Parafii rozpoczną się rekolekcje wielkopostne.
+            Przeprowadzi je&nbsp;Ks.&nbsp;prof. Adam Kopeć, dyrektor
+            Wydziału Katechetyki przy Kurii Diecezjalnej, który z&nbsp;nami
+            współpracuje w&nbsp;duszpasterstwie. Módlmy się dla niego
+            o&nbsp;dary Ducha świętego. Ogłaszamy wcześniej abyśmy mogli
+            zaplanować sobie czas na&nbsp;spotkania rekolekcyjne.</p>
+        <p>2. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
+            z&nbsp;naszej parafii Stanisławę Teter i&nbsp;Wincentego
+            Świrkulę. Polećmy ich Miłosierdziu Bożemu odmawiając:
+            <em>Wieczny odpoczynek...</em></p>
+        <p>3. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę przez sprzątających, dary stołu i&nbsp;wszelką życzliwość.<br />
             <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
             na&nbsp;godz.&nbsp;7.00</strong> Rodziny z&nbsp;bloku
-            Maciejowskiego 7 od&nbsp;31 do&nbsp;40.</p>
-        <p>5. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>6. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            Maciejowskiego 7 od&nbsp;41 do&nbsp;50. <em>Bóg zapłać</em> Panu
+            Ryszardowi za&nbsp;pracę dla dobra Parafii.</p>
+        <p>4. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy
+            katolickiej.</p>
+        <p>5. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
             innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
             Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
             przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>7. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>6. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
             urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
             jak również ludziom starszym, schorowanym, którzy już osobiście
             nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
@@ -45,31 +37,8 @@
             i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
             intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 9.&nbsp;lutego 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 16.&nbsp;lutego 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
-    </div>
-</div>
-<br />
-
-<div class="card">
-    <div class="card-header">
-        <h3>Ksiądz Dyrektor diecezjalnej Caritas informuje
-            i&nbsp;zaprasza:</h3>
-    </div>
-    <div class="col">
-        <p>We&nbsp;wtorek wspomnienie NMP z&nbsp;Lourdes, XXVIII
-            Światowy Dzień Chorego. Diecezjalne obchody XXVIII
-            Światowego Dnia Chorego pod przewodnictwem Ks.&nbsp;Bpa
-            Ordynariusza Krzysztofa Nitkiewicza odbędą się
-            w&nbsp;Sandomierzu w&nbsp;Kościele Seminaryjnym
-            pw.&nbsp;Św.&nbsp;Michała Archanioła,
-            o&nbsp;godz.&nbsp;10.00 Msza Święta z&nbsp;możliwością
-            przyjęcia sakramentu chorych i&nbsp;błogosławieństwo
-            Najświętszym Sakramentem, o&nbsp;godz.&nbsp;11.30,
-            w&nbsp;Domu Katolickim koncert Pani Halny Frąckowiak
-            zapraszamy osoby chore, niepełnosprawne wraz
-            ze&nbsp;swoimi opiekunami oraz pracowników służby zdrowia
-            i&nbsp;ich duszpasterzy do&nbsp;wspólnej modlitwy.</p>
     </div>
 </div>
