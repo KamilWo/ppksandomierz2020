6b6f807fa1b5ce5e697ee25c7e253a0b506bf41a
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-02-23 09:55:33 +0000

    2020-02-23

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,53 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 9.&nbsp;lutego 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 16.&nbsp;lutego 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>16.&nbsp;lutego 2020&nbsp;r.</h3>
+        <h3>VI Niedziela Zwykła - Rok A</h3>
+    </div>
+    <div class="col">
+        <p>1. Kochani, kończy się czas karnawału, rozpocznie się czas
+            Wielkiego Postu, przyszła środa to&nbsp;Środa Popielcowa.
+            U&nbsp;nas w&nbsp;Parafii rozpoczną się rekolekcje wielkopostne.
+            Przeprowadzi je&nbsp;Ks.&nbsp;prof. Adam Kopeć, dyrektor
+            Wydziału Katechetyki przy Kurii Diecezjalnej, który z&nbsp;nami
+            współpracuje w&nbsp;duszpasterstwie. Módlmy się dla niego
+            o&nbsp;dary Ducha świętego. Ogłaszamy wcześniej abyśmy mogli
+            zaplanować sobie czas na&nbsp;spotkania rekolekcyjne.</p>
+        <p>2. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
+            z&nbsp;naszej parafii Stanisławę Teter i&nbsp;Wincentego
+            Świrkulę. Polećmy ich Miłosierdziu Bożemu odmawiając:
+            <em>Wieczny odpoczynek...</em></p>
+        <p>3. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę przez sprzątających, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+                na&nbsp;godz.&nbsp;7.00</strong> Rodziny z&nbsp;bloku
+            Maciejowskiego 7 od&nbsp;41 do&nbsp;50. <em>Bóg zapłać</em> Panu
+            Ryszardowi za&nbsp;pracę dla dobra Parafii.</p>
+        <p>4. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy
+            katolickiej.</p>
+        <p>5. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
+            Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
+            przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>6. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+            jak również ludziom starszym, schorowanym, którzy już osobiście
+            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+            we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
+            i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
+            intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 16.&nbsp;lutego 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>9.&nbsp;lutego 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,43 +1,81 @@
 <div class="card">
     <div class="card-header">
-        <h3>16.&nbsp;lutego 2020&nbsp;r.</h3>
-        <h3>VI Niedziela Zwykła - Rok A</h3>
+        <h3>23.&nbsp;lutego 2020&nbsp;r.</h3>
+        <h3>VII Niedziela Zwykła - Rok A</h3>
     </div>
     <div class="col">
-        <p>1. Kochani, kończy się czas karnawału, rozpocznie się czas
-            Wielkiego Postu, przyszła środa to&nbsp;Środa Popielcowa.
-            U&nbsp;nas w&nbsp;Parafii rozpoczną się rekolekcje wielkopostne.
-            Przeprowadzi je&nbsp;Ks.&nbsp;prof. Adam Kopeć, dyrektor
-            Wydziału Katechetyki przy Kurii Diecezjalnej, który z&nbsp;nami
-            współpracuje w&nbsp;duszpasterstwie. Módlmy się dla niego
-            o&nbsp;dary Ducha świętego. Ogłaszamy wcześniej abyśmy mogli
-            zaplanować sobie czas na&nbsp;spotkania rekolekcyjne.</p>
-        <p>2. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
-            z&nbsp;naszej parafii Stanisławę Teter i&nbsp;Wincentego
-            Świrkulę. Polećmy ich Miłosierdziu Bożemu odmawiając:
-            <em>Wieczny odpoczynek...</em></p>
-        <p>3. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
-            ofiarę przez sprzątających, dary stołu i&nbsp;wszelką życzliwość.<br />
+        <p>1. We&nbsp;wtorek otrzymaliśmy pismo z&nbsp;Kurii Diecezjalnej,
+            w&nbsp;którym Ks.&nbsp;Biskup Ordynariusz odwołał
+            ze&nbsp;stanowiska wikariusza naszej Parafii ks.&nbsp;doktoranta
+            Damiana Dziubę i&nbsp;przeniósł do&nbsp;Parafii Padew Narodowa.
+            Na&nbsp;jego miejsce został zamianowany ks.&nbsp;mgr Piotr
+            Cieśla. <em>Bóg zapłać</em> ks.&nbsp;Damianowi za&nbsp;jego
+            gorliwą pracę w&nbsp;naszej Parafii. Księdza Piotra witamy
+            serdecznie w&nbsp;naszym zespole i&nbsp;polecamy Bogu jego osobę
+            i&nbsp;pracę w&nbsp;naszych modlitwach.</p>
+        <p>2. Kochani, w&nbsp;tym tygodniu rozpoczynamy czas Wielkiego
+            Postu, najbliższa środa to&nbsp;Środa Popielcowa. Msze&nbsp;św.
+            z&nbsp;posypaniem głów popiołem o&nbsp;godz.&nbsp;6.30, 9.00,
+            16.00 i&nbsp;18.00. Przypominamy, że&nbsp;w&nbsp;tym dniu
+            obowiązuje post ścisły ilościowy i&nbsp;jakościowy. <strong>Taca
+            ze&nbsp;Środy Popielcowej przeznaczona jest
+            na&nbsp;Caritas.</strong> W&nbsp;naszej Parafii rozpoczną się
+            rekolekcje wielkopostne. Przeprowadzi je&nbsp;Ks.&nbsp;prof.
+            Adam Kopeć, dyrektor Wydziału Katechetyki przy Kurii
+            Diecezjalnej, który z&nbsp;nami współpracuje
+            w&nbsp;duszpasterstwie. Módlmy się dla niego o&nbsp;dary Ducha
+            świętego. Program rekolekcji wisi na&nbsp;drzwiach kościoła;
+            proszę Radnych Parafialnych i&nbsp;Liderów blokowych
+            o&nbsp;wywieszenie go&nbsp;w&nbsp;klatkach bloków
+            i&nbsp;na&nbsp;tablicach ogłoszeniowych na&nbsp;terenie
+            Parafii.</p>
+        <p>3. We&nbsp;wtorek od&nbsp;godziny 16.00 będzie w&nbsp;naszym
+            kościele wystawiony Najświętszy Sakrament do&nbsp;adoracji.
+            Prowadzą modlitwę Panie z&nbsp;Legionu Maryi w&nbsp;intencji
+            głębokiego przeżycia rekolekcji i&nbsp;o&nbsp;powołania
+            kapłańskie zakonne i&nbsp;misyjne.</p>
+        <p>4. Stacyjna Droga Krzyżowa będzie w&nbsp;piątek o&nbsp;godzinie
+            21.00 w&nbsp;kościele p.w.&nbsp;Św.&nbsp;Michała.</p>
+        <p>5. Msza Święta w&nbsp;intencjach Pana Mirka, naszego Organisty,
+            który ma&nbsp;swoje Imieniny w&nbsp;środę, będzie odprawiona
+            o&nbsp;godzinie 16.00. Zapraszamy do&nbsp;wspólnej modlitwy
+            w&nbsp;intencjach Solenizanta.</p>
+        <p>6. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
+            małżeństwa rozpocznie się w&nbsp;Katedrze Mszą Świętą
+            o&nbsp;godzinie 10.30 w&nbsp;I&nbsp;niedzielę Wielkiego
+            Postu.</strong></p>
+        <p>7. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej parafii
+            do&nbsp;wieczności Stanisława Kołodzieja. Polećmy
+            go&nbsp;Miłosierdziu Bożemu odmawiając: <em>Wieczny
+                odpoczynek...</em></p>
+        <p>8. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę przez sprzątających, dary stołu i&nbsp;wszelką
+            życzliwość.<br />
             <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-            na&nbsp;godz.&nbsp;7.00</strong> Rodziny z&nbsp;bloku
-            Maciejowskiego 7 od&nbsp;41 do&nbsp;50. <em>Bóg zapłać</em> Panu
-            Ryszardowi za&nbsp;pracę dla dobra Parafii.</p>
-        <p>4. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+                na&nbsp;godz.&nbsp;7.00</strong> Rodziny z&nbsp;bloku
+            Maciejowskiego 4 od&nbsp;1 do&nbsp;10 włącznie. <em>Bóg
+                zapłać</em> Panu Ryszardowi i&nbsp;Tadeuszowi za&nbsp;pracę dla
+            dobra Parafii.</p>
+        <p>9. Spotkanie dla kandydatów do&nbsp;Sakramentu Bierzmowania
+            z&nbsp;kl.&nbsp;8 we&nbsp;wtorek o&nbsp;godzinie 17.00.</p>
+        <p>10. Spotkanie grupy Odnowy w&nbsp;Duchu Świętym we&nbsp;wtorek
+            po&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;18.00.</p>
+        <p>11. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
             Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy
             katolickiej.</p>
-        <p>5. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        <p>12. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
             innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
             Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
             przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>6. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
-            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
-            jak również ludziom starszym, schorowanym, którzy już osobiście
-            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje
+            imieniny, urodziny, jakiekolwiek rocznice, gościom, uczestnikom
+            liturgii, jak również ludziom starszym, schorowanym, którzy już
+            osobiście nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
             we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
             i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
             intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 16.&nbsp;lutego 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 23.&nbsp;lutego 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
