625d737aa6e865529bfb4fdc88c6adc4b027658f
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-03-03 18:31:07 +0000

    2020-03-01

--- last_post.php
+++ last_post.php
@@ -1,142 +1,72 @@
 <div class="card">
     <div class="card-header">
-        <h3>Rekolekcje Wielkopostne<br/>
-            w&nbsp;Parafii Podwyższenia Krzyża Świętego<br/>
-            w&nbsp;Sandomierzu w&nbsp;roku 2020</h3>
+        <h3>1.&nbsp;marca 2020&nbsp;r.</h3>
+        <h3>I NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
     </div>
     <div class="col">
-        <p class="text-underline"><strong>Środa Popielcowa, 26&nbsp;lutego</strong></p>
-        <ul>
-            <li>9.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich i&nbsp;posypanie głów popiołem</li>
-            <li>16.00 – Msza&nbsp;św. z&nbsp;nauką dla dzieci szkolnych i&nbsp;posypanie głów popiołem</li>
-            <li>17.30 – Różaniec w intencji parafian</li>
-            <li>18.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich i&nbsp;posypanie głów popiołem</li>
-        </ul>
-
-        <p class="text-underline"><strong>Czwartek, 27&nbsp;lutego</strong></p>
-        <ul>
-            <li>9.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich</li>
-            <li>17.30 – Różaniec w&nbsp;intencji parafian</li>
-            <li>18.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich</li>
-        </ul>
-
-        <p class="text-underline"><strong>Piątek, 28&nbsp;lutego – DZIEŃ SPOWIEDZI</strong></p>
-        <p class="text-indent"><strong>(w&nbsp;godzinach: 8.30 - 10.00 i&nbsp;od&nbsp;16.30 – 17.30)</strong></p>
-        <ul>
-            <li>9.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich – spowiadamy od&nbsp;godz.&nbsp;8.30</li>
-            <li>17.00 – Droga Krzyżowa dla wszystkich parafian (dzieci, młodzież, dorośli)</li>
-            <li>18:00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich</li>
-        </ul>
-
-        <p class="text-underline"><strong>Sobota, 29&nbsp;lutego – DZIEŃ ADORACJI NAJŚW. SAKRAMENTU</strong></p>
-        <ul>
-            <li>6.30 – Msza&nbsp;św. i&nbsp;wystawienie Najświętszego Sakramentu</li>
-            <li>9.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich</li>
-            <li>11.00 – Msza&nbsp;św. z&nbsp;nauką dla chorych, emerytów, rencistów (udzielamy sakramentu chorych),
-                błogosławieństwo lourdzkie
-            </li>
-            <li>17.30 – Różaniec w intencji parafian i&nbsp;zakończenie adoracji Najświętszego Sakramentu</li>
-            <li>18.00 – Msza&nbsp;św. z&nbsp;nauką dla wszystkich</li>
-        </ul>
-
-        <p class="text-underline"><strong>Niedziela, 1&nbsp;marca 2020 - I&nbsp;WIELKIEGO POSTU</strong></p>
-        <ul>
-            <li>7.30, 9.30, 11.15, 12.30, 16.30, 19.30 – Msza&nbsp;św. z&nbsp;nauką rekolekcyjną</li>
-            <li>15.30 – Gorzkie Żale z&nbsp;kazaniem pasyjnym</li>
-        </ul>
-
-        <p style="float: right; text-align: right;">
-            <strong>Rekolekcje prowadzi ks.&nbsp;dr&nbsp;Adam Kopeć</strong><br/>
-            Dyrektor Wydziału Katechetyki przy Kurii Diecezjalnej.
-        </p>
-        <p class="clear"></p>
-        <p>
-            <span style="float: left;">Sandomierz, 23.&nbsp;lutego 2020&nbsp;r.</span>
-            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
-        </p>
-    </div>
-</div>
-<br/>
-
-
-<div class="card">
-    <div class="card-header">
-        <h3>23.&nbsp;lutego 2020&nbsp;r.</h3>
-        <h3>VII Niedziela Zwykła - Rok A</h3>
-    </div>
-    <div class="col">
-        <p>1. We&nbsp;wtorek otrzymaliśmy pismo z&nbsp;Kurii Diecezjalnej,
-            w&nbsp;którym Ks.&nbsp;Biskup Ordynariusz odwołał
-            ze&nbsp;stanowiska wikariusza naszej Parafii ks.&nbsp;doktoranta
-            Damiana Dziubę i&nbsp;przeniósł do&nbsp;Parafii Padew Narodowa.
-            Na&nbsp;jego miejsce został zamianowany ks.&nbsp;mgr Piotr
-            Cieśla. <em>Bóg zapłać</em> ks.&nbsp;Damianowi za&nbsp;jego
-            gorliwą pracę w&nbsp;naszej Parafii. Księdza Piotra witamy
-            serdecznie w&nbsp;naszym zespole i&nbsp;polecamy Bogu jego osobę
-            i&nbsp;pracę w&nbsp;naszych modlitwach.</p>
-        <p>2. Kochani, w&nbsp;tym tygodniu rozpoczynamy czas Wielkiego
-            Postu, najbliższa środa to&nbsp;Środa Popielcowa. Msze&nbsp;św.
-            z&nbsp;posypaniem głów popiołem o&nbsp;godz.&nbsp;6.30, 9.00,
-            16.00 i&nbsp;18.00. Przypominamy, że&nbsp;w&nbsp;tym dniu
-            obowiązuje post ścisły ilościowy i&nbsp;jakościowy. <strong>Taca
-            ze&nbsp;Środy Popielcowej przeznaczona jest
-            na&nbsp;Caritas.</strong> W&nbsp;naszej Parafii rozpoczną się
-            rekolekcje wielkopostne. Przeprowadzi je&nbsp;Ks.&nbsp;prof.
-            Adam Kopeć, dyrektor Wydziału Katechetyki przy Kurii
-            Diecezjalnej, który z&nbsp;nami współpracuje
-            w&nbsp;duszpasterstwie. Módlmy się dla niego o&nbsp;dary Ducha
-            świętego. Program rekolekcji wisi na&nbsp;drzwiach kościoła;
-            proszę Radnych Parafialnych i&nbsp;Liderów blokowych
-            o&nbsp;wywieszenie go&nbsp;w&nbsp;klatkach bloków
-            i&nbsp;na&nbsp;tablicach ogłoszeniowych na&nbsp;terenie
-            Parafii.</p>
-        <p>3. We&nbsp;wtorek od&nbsp;godziny 16.00 będzie w&nbsp;naszym
-            kościele wystawiony Najświętszy Sakrament do&nbsp;adoracji.
-            Prowadzą modlitwę Panie z&nbsp;Legionu Maryi w&nbsp;intencji
-            głębokiego przeżycia rekolekcji i&nbsp;o&nbsp;powołania
-            kapłańskie zakonne i&nbsp;misyjne.</p>
+        <h3><em>Bóg zapłać</em> tym wszystkim Parafianom, którzy
+            uczestniczyli w&nbsp;rekolekcjach wielkopostnych. Umocnieni
+            Słowem Boga i&nbsp;Ciałem Jezusa Chrystusa trwajmy
+            w&nbsp;przyjaźni z&nbsp;Nim i&nbsp;między sobą, dając piękny
+            przykład postępowania przez cały post i&nbsp;całe nasze życie.
+            Słowa serdecznego podziękowania za&nbsp;przeprowadzenie
+            rekolekcji składamy ks.&nbsp;prof. Adamowi, życząc
+            mu&nbsp;zdrowia i&nbsp;darów Ducha świętego w&nbsp;wypełnianiu
+            posługi dla dobra Diecezji.</h3>
+        <p>1. Dzisiaj o&nbsp;15.30 Gorzkie Żale z&nbsp;nauką rekolekcyjną,
+            następnie Msza Święta i&nbsp;zmiana tajemnic różańcowych dla
+            członków Kół Żywego Różańca.</p>
+        <p>2. W&nbsp;tym tygodniu wypada I&nbsp;czwartek, piątek
+            i&nbsp;sobota miesiąca marca. Odwiedzimy chorych z&nbsp;Komunią
+            Świętą.</p>
+        <p>3. Droga Krzyżowa w&nbsp;piątek dla dzieci o&nbsp;16.30, dla
+            dorosłych, 17.30 i&nbsp;dla młodzieży o&nbsp;19.00. Zapraszamy
+            do&nbsp;licznego udziału. Gorzkie Żale z&nbsp;nauką pasyjną będą
+            w&nbsp;niedziele o&nbsp;godzinie 15.30.</p>
         <p>4. Stacyjna Droga Krzyżowa będzie w&nbsp;piątek o&nbsp;godzinie
-            21.00 w&nbsp;kościele p.w.&nbsp;Św.&nbsp;Michała.</p>
-        <p>5. Msza Święta w&nbsp;intencjach Pana Mirka, naszego Organisty,
-            który ma&nbsp;swoje Imieniny w&nbsp;środę, będzie odprawiona
-            o&nbsp;godzinie 16.00. Zapraszamy do&nbsp;wspólnej modlitwy
-            w&nbsp;intencjach Solenizanta.</p>
-        <p>6. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
-            małżeństwa rozpocznie się w&nbsp;Katedrze Mszą Świętą
-            o&nbsp;godzinie 10.30 w&nbsp;I&nbsp;niedzielę Wielkiego
-            Postu.</strong></p>
-        <p>7. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej parafii
-            do&nbsp;wieczności Stanisława Kołodzieja. Polećmy
-            go&nbsp;Miłosierdziu Bożemu odmawiając: <em>Wieczny
-            odpoczynek...</em></p>
-        <p>8. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
-            ofiarę przez sprzątających, dary stołu i&nbsp;wszelką
-            życzliwość.<br/>
-            <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-            na&nbsp;godz.&nbsp;7.00</strong> Rodziny z&nbsp;bloku
-            Maciejowskiego 4 od&nbsp;1 do&nbsp;10 włącznie. <em>Bóg
-            zapłać</em> Panu Ryszardowi i&nbsp;Tadeuszowi za&nbsp;pracę dla
-            dobra Parafii.</p>
-        <p>9. Spotkanie dla kandydatów do&nbsp;Sakramentu Bierzmowania
-            z&nbsp;kl.&nbsp;8 we&nbsp;wtorek o&nbsp;godzinie 17.00.</p>
-        <p>10. Spotkanie grupy Odnowy w&nbsp;Duchu Świętym we&nbsp;wtorek
+            17.00 w&nbsp;Katedrze.</p>
+        <p>5. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
+            małżeństwa rozpoczyna się dzisiaj w&nbsp;Katedrze Mszą Świętą
+            o&nbsp;godzinie 10.30.</strong></p>
+        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej parafii
+            do&nbsp;wieczności Franciszka Makowskiego, Janinę Zybała, Jacka
+            Lipca, Zdzisława Olejniczaka, którego pogrzeb będzie jutro
+            o&nbsp;11.00 i&nbsp;Helenę Przyłucką – pogrzeb w&nbsp;środę
+            o&nbsp;14.00. Polećmy ich Miłosierdziu Bożemu odmawiając:
+            <em>Wieczny odpoczynek...</em></p>
+        <p>7. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną ofiarę
+            przez sprzątających, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;piątek
+            na&nbsp;godz.&nbsp;18.30</strong> rodziców kandydatów
+            do&nbsp;bierzmowania i&nbsp;młodzież oczekującą na&nbsp;dary
+            Ducha Świętego. <em>Bóg zapłać</em> Panu Ryszardowi
+            i&nbsp;Tadeuszowi za&nbsp;pracę dla dobra Parafii.</p>
+        <p>8. Spotkanie dla kandydatów do&nbsp;Sakramentu Bierzmowania
+            z&nbsp;kl.&nbsp;8 codziennie o&nbsp;godzinie 16.00. W&nbsp;sobotę
+            o&nbsp;godz.&nbsp;9.30 DZIEŃ SKUPIENIA dla kandydatów, rodziców
+            i&nbsp;świadków połączony ze&nbsp;spowiedzią świętą. Bierzmowanie
+            w&nbsp;poniedziałek 9&nbsp;marca o&nbsp;godzinie 18.00.
+            Szafarzem Sakramentu będzie Pasterz Diecezji Bp&nbsp;Krzysztof
+            Nitkiewicz.</p>
+        <p>9. Spotkanie grupy Odnowy w&nbsp;Duchu Świętym we&nbsp;wtorek
             po&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;18.00.</p>
-        <p>11. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy
-            katolickiej.</p>
+        <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>11. W&nbsp;przyszłą niedzielę zbiórka do&nbsp;puszek na&nbsp;rzecz
+            dzieła pomocy misjonarzom "Ad gentes".</p>
         <p>12. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
             innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
             Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
             przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje
-            imieniny, urodziny, jakiekolwiek rocznice, gościom, uczestnikom
-            liturgii, jak również ludziom starszym, schorowanym, którzy już
-            osobiście nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+            jak również ludziom starszym, schorowanym, którzy już osobiście
+            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
             we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
             i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
             intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 23.&nbsp;lutego 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 1.&nbsp;marca 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
