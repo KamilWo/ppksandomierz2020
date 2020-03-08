b3e3a9118cd221ba0c3a7f60b0bebd11249808fe
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-03-08 08:48:31 +0000

    2020-03-08

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,82 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 23.&nbsp;lutego 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 1.&nbsp;marca 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>1.&nbsp;marca 2020&nbsp;r.</h3>
+        <h3>I NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
+    </div>
+    <div class="col">
+        <h3><em>Bóg zapłać</em> tym wszystkim Parafianom, którzy
+            uczestniczyli w&nbsp;rekolekcjach wielkopostnych. Umocnieni
+            Słowem Boga i&nbsp;Ciałem Jezusa Chrystusa trwajmy
+            w&nbsp;przyjaźni z&nbsp;Nim i&nbsp;między sobą, dając piękny
+            przykład postępowania przez cały post i&nbsp;całe nasze życie.
+            Słowa serdecznego podziękowania za&nbsp;przeprowadzenie
+            rekolekcji składamy ks.&nbsp;prof. Adamowi, życząc
+            mu&nbsp;zdrowia i&nbsp;darów Ducha świętego w&nbsp;wypełnianiu
+            posługi dla dobra Diecezji.</h3>
+        <p>1. Dzisiaj o&nbsp;15.30 Gorzkie Żale z&nbsp;nauką rekolekcyjną,
+            następnie Msza Święta i&nbsp;zmiana tajemnic różańcowych dla
+            członków Kół Żywego Różańca.</p>
+        <p>2. W&nbsp;tym tygodniu wypada I&nbsp;czwartek, piątek
+            i&nbsp;sobota miesiąca marca. Odwiedzimy chorych z&nbsp;Komunią
+            Świętą.</p>
+        <p>3. Droga Krzyżowa w&nbsp;piątek dla dzieci o&nbsp;16.30, dla
+            dorosłych, 17.30 i&nbsp;dla młodzieży o&nbsp;19.00. Zapraszamy
+            do&nbsp;licznego udziału. Gorzkie Żale z&nbsp;nauką pasyjną będą
+            w&nbsp;niedziele o&nbsp;godzinie 15.30.</p>
+        <p>4. Stacyjna Droga Krzyżowa będzie w&nbsp;piątek o&nbsp;godzinie
+            17.00 w&nbsp;Katedrze.</p>
+        <p>5. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
+                małżeństwa rozpoczyna się dzisiaj w&nbsp;Katedrze Mszą Świętą
+                o&nbsp;godzinie 10.30.</strong></p>
+        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej parafii
+            do&nbsp;wieczności Franciszka Makowskiego, Janinę Zybała, Jacka
+            Lipca, Zdzisława Olejniczaka, którego pogrzeb będzie jutro
+            o&nbsp;11.00 i&nbsp;Helenę Przyłucką – pogrzeb w&nbsp;środę
+            o&nbsp;14.00. Polećmy ich Miłosierdziu Bożemu odmawiając:
+            <em>Wieczny odpoczynek...</em></p>
+        <p>7. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną ofiarę
+            przez sprzątających, dary stołu i&nbsp;wszelką życzliwość.<br />
+            <strong>Do&nbsp;sprzątania prosimy w&nbsp;piątek
+                na&nbsp;godz.&nbsp;18.30</strong> rodziców kandydatów
+            do&nbsp;bierzmowania i&nbsp;młodzież oczekującą na&nbsp;dary
+            Ducha Świętego. <em>Bóg zapłać</em> Panu Ryszardowi
+            i&nbsp;Tadeuszowi za&nbsp;pracę dla dobra Parafii.</p>
+        <p>8. Spotkanie dla kandydatów do&nbsp;Sakramentu Bierzmowania
+            z&nbsp;kl.&nbsp;8 codziennie o&nbsp;godzinie 16.00. W&nbsp;sobotę
+            o&nbsp;godz.&nbsp;9.30 DZIEŃ SKUPIENIA dla kandydatów, rodziców
+            i&nbsp;świadków połączony ze&nbsp;spowiedzią świętą.
+            <strong>Bierzmowanie w&nbsp;poniedziałek 9&nbsp;marca o&nbsp;godzinie
+                18.00. Szafarzem Sakramentu będzie Pasterz Diecezji
+                Bp&nbsp;Krzysztof Nitkiewicz.</strong></p>
+        <p>9. Spotkanie grupy Odnowy w&nbsp;Duchu Świętym we&nbsp;wtorek
+            po&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;18.00.</p>
+        <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>11. W&nbsp;przyszłą niedzielę zbiórka do&nbsp;puszek na&nbsp;rzecz
+            dzieła pomocy misjonarzom "Ad gentes".</p>
+        <p>12. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+            innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
+            Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
+            przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+            jak również ludziom starszym, schorowanym, którzy już osobiście
+            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+            we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
+            i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
+            intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 1.&nbsp;marca 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>Rekolekcje Wielkopostne<br/>
--- last_post.php
+++ last_post.php
@@ -1,72 +1,52 @@
 <div class="card">
     <div class="card-header">
-        <h3>1.&nbsp;marca 2020&nbsp;r.</h3>
-        <h3>I NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
+        <h3>8.&nbsp;marca 2020&nbsp;r.</h3>
+        <h3>II NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
     </div>
     <div class="col">
-        <h3><em>Bóg zapłać</em> tym wszystkim Parafianom, którzy
-            uczestniczyli w&nbsp;rekolekcjach wielkopostnych. Umocnieni
-            Słowem Boga i&nbsp;Ciałem Jezusa Chrystusa trwajmy
-            w&nbsp;przyjaźni z&nbsp;Nim i&nbsp;między sobą, dając piękny
-            przykład postępowania przez cały post i&nbsp;całe nasze życie.
-            Słowa serdecznego podziękowania za&nbsp;przeprowadzenie
-            rekolekcji składamy ks.&nbsp;prof. Adamowi, życząc
-            mu&nbsp;zdrowia i&nbsp;darów Ducha świętego w&nbsp;wypełnianiu
-            posługi dla dobra Diecezji.</h3>
-        <p>1. Dzisiaj o&nbsp;15.30 Gorzkie Żale z&nbsp;nauką rekolekcyjną,
-            następnie Msza Święta i&nbsp;zmiana tajemnic różańcowych dla
-            członków Kół Żywego Różańca.</p>
-        <p>2. W&nbsp;tym tygodniu wypada I&nbsp;czwartek, piątek
-            i&nbsp;sobota miesiąca marca. Odwiedzimy chorych z&nbsp;Komunią
-            Świętą.</p>
-        <p>3. Droga Krzyżowa w&nbsp;piątek dla dzieci o&nbsp;16.30, dla
+        <p>1. Dzisiaj o&nbsp;15.30 Gorzkie Żale z&nbsp;kazaniem pasyjnym,
+            następnie Msza Święta.</p>
+        <p>2. Droga Krzyżowa w&nbsp;piątek dla dzieci o&nbsp;16.30, dla
             dorosłych, 17.30 i&nbsp;dla młodzieży o&nbsp;19.00. Zapraszamy
-            do&nbsp;licznego udziału. Gorzkie Żale z&nbsp;nauką pasyjną będą
-            w&nbsp;niedziele o&nbsp;godzinie 15.30.</p>
-        <p>4. Stacyjna Droga Krzyżowa będzie w&nbsp;piątek o&nbsp;godzinie
-            17.00 w&nbsp;Katedrze.</p>
-        <p>5. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
-            małżeństwa rozpoczyna się dzisiaj w&nbsp;Katedrze Mszą Świętą
-            o&nbsp;godzinie 10.30.</strong></p>
-        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej parafii
-            do&nbsp;wieczności Franciszka Makowskiego, Janinę Zybała, Jacka
-            Lipca, Zdzisława Olejniczaka, którego pogrzeb będzie jutro
-            o&nbsp;11.00 i&nbsp;Helenę Przyłucką – pogrzeb w&nbsp;środę
-            o&nbsp;14.00. Polećmy ich Miłosierdziu Bożemu odmawiając:
+            do&nbsp;licznego udziału. Stacyjna Droga Krzyżowa będzie
+            w&nbsp;piątek o&nbsp;godzinie 17.00 w&nbsp;kościele Chrystusa
+            Króla.</p>
+        <p>3. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
+            małżeństwa trwa w&nbsp;Parafii katedralnej.</strong></p>
+        <p>4. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej parafii
+            do&nbsp;wieczności Lucynę Krzystanek-Słyk i&nbsp;Zenona
+            Królikowskiego. Polećmy ich Miłosierdziu Bożemu odmawiając:
             <em>Wieczny odpoczynek...</em></p>
-        <p>7. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną ofiarę
-            przez sprzątających, dary stołu i&nbsp;wszelką życzliwość.<br />
-            <strong>Do&nbsp;sprzątania prosimy w&nbsp;piątek
-            na&nbsp;godz.&nbsp;18.30</strong> rodziców kandydatów
-            do&nbsp;bierzmowania i&nbsp;młodzież oczekującą na&nbsp;dary
-            Ducha Świętego. <em>Bóg zapłać</em> Panu Ryszardowi
-            i&nbsp;Tadeuszowi za&nbsp;pracę dla dobra Parafii.</p>
-        <p>8. Spotkanie dla kandydatów do&nbsp;Sakramentu Bierzmowania
-            z&nbsp;kl.&nbsp;8 codziennie o&nbsp;godzinie 16.00. W&nbsp;sobotę
-            o&nbsp;godz.&nbsp;9.30 DZIEŃ SKUPIENIA dla kandydatów, rodziców
-            i&nbsp;świadków połączony ze&nbsp;spowiedzią świętą. Bierzmowanie
-            w&nbsp;poniedziałek 9&nbsp;marca o&nbsp;godzinie 18.00.
-            Szafarzem Sakramentu będzie Pasterz Diecezji Bp&nbsp;Krzysztof
-            Nitkiewicz.</p>
-        <p>9. Spotkanie grupy Odnowy w&nbsp;Duchu Świętym we&nbsp;wtorek
+        <p>5. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+            ofiarę Rodzicom młodzieży, która jutro przystąpi
+            do&nbsp;Sakramentu Bierzmowania, którego szafarzem
+            <strong>będzie Pasterz diecezji, ks.&nbsp;biskup Krzysztof
+            Nitkiewicz</strong>. <em>Bóg zapłać</em> Panu Wacławowi,
+            Ryszardowi i&nbsp;Tadeuszowi za&nbsp;pracę dla dobra Parafii.</p>
+        <p>6. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+            na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku
+            Maciejowskiego 6 od&nbsp;1 do&nbsp;10 włącznie.</p>
+        <p>7. Spotkanie grupy Odnowy w&nbsp;Duchu Świętym we&nbsp;wtorek
             po&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;18.00.</p>
-        <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>11. W&nbsp;przyszłą niedzielę zbiórka do&nbsp;puszek na&nbsp;rzecz
-            dzieła pomocy misjonarzom "Ad gentes".</p>
-        <p>12. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        <p>8. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+            Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy
+            katolickiej.</p>
+        <p>9. Dzisiaj wychodząc z&nbsp;kościoła możemy złożyć ofiarę
+            do&nbsp;puszek na&nbsp;rzecz dzieła pomocy misjonarzom
+            "Ad&nbsp;gentes".</p>
+        <p>10. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
             innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji,
             Węgrzech. Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie
             przy wejściu do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
-            urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
-            jak również ludziom starszym, schorowanym, którzy już osobiście
-            nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
+        <p>11. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje
+            imieniny, urodziny, jakiekolwiek rocznice, gościom, uczestnikom
+            liturgii, jak również ludziom starszym, schorowanym, którzy już
+            osobiście nie mogą uczestniczyć z&nbsp;nami w&nbsp;kościele
             we&nbsp;Mszy&nbsp;św., składamy najlepsze życzenia
             i&nbsp;zapewniamy o&nbsp;naszej modlitwie w&nbsp;waszych
             intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 1.&nbsp;marca 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 8.&nbsp;marca 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
