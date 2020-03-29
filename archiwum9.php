b770ee11951c9cc21532b37fba4bb75efeefd0fd
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-03-29 09:23:40 +0100

    2020-03-29

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,53 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 15.&nbsp;marca 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 22.&nbsp;marca 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>22.&nbsp;marca 2020&nbsp;r.</h3>
+        <h3>IV NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
+    </div>
+    <div class="col">
+        <p>1. Dzisiaj porządek Mszy Świętych w&nbsp;naszym kościele jak
+        w&nbsp;każdą niedzielę. Zgodnie z&nbsp;zaleceniem Kurii Diecezjalnej
+        nie będzie nabożeństwa Gorzkich Zali. W&nbsp;piątek nie będzie
+        również nabożeństwa Drogi Krzyżowej. Ks.&nbsp;bp&nbsp;udzielił
+        dyspensy od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
+        do&nbsp;odwołania. Zachęca nas jednak do udziału we&nbsp;Mszy
+        Świętej i&nbsp;w&nbsp;nabożeństwach wielkopostnych korzystając
+        z&nbsp;mediów zarówno ogólnopolskich, jak i&nbsp;lokalnych.</p>
+        <p>2. Kościoły są&nbsp;otwarte, zachęcamy do&nbsp;modlitwy przed
+        Najświętszym Sakramentem o&nbsp;ustanie epidemii.</p>
+        <p>3. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
+        wspólnoty do&nbsp;wieczności Reginę Jakubczyk. <em>Wieczny
+        odpoczynek...</em></p>
+        <p>4. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
+        małżeństwa została odwołana z&nbsp;racji epidemii.</strong></p>
+        <p>5. <em>Bóg zapłać</em> Panu Ryszardowi i&nbsp;Tadeuszowi
+        za&nbsp;pracę dla dobra Parafii.</p>
+        <p>6. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
+        od&nbsp;21 do&nbsp;30 włącznie.</p>
+        <p>7. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
+        Zachęcamy do&nbsp;nabywania baranków wielkanocnych – Akcja
+        Caritas.</p>
+        <p>8. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech.
+        Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
+        do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>9. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej
+        modlitwie w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 22.&nbsp;marca 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>15.&nbsp;marca 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,5 +1,27 @@
 <div class="card">
     <div class="card-header">
+        <h3>PRZYPOMINAMY</h3>
+    </div>
+    <div class="col">
+        <h4>
+            PRZYPOMINAMY, ŻE&nbsp;ZGODNIE Z&nbsp;OBOWIĄZUJĄCYMI
+            OBOSTRZENIAMI I&nbsp;ZARZĄDZENIEM KURII DIECEZJALNEJ
+            W&nbsp;SANDOMIERZU W&nbsp;KOŚCIELE W&nbsp;CZASIE
+            ZGROMADZEŃ LITURGICZNYCH MOŻE PRZEBYWAĆ<br /><br />
+
+            MAKSYMALNIE 5&nbsp;OSÓB<br /><br />
+            PIERWSZEŃSTWO MAJĄ OSOBY, KTÓRE ZAMÓWIŁY INTENCJE MSZALNĄ.
+        </h4>
+        <p>
+            <span style="float: left;">Sandomierz, 29.&nbsp;marca 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
+<div class="card">
+    <div class="card-header">
         <h3>19.&nbsp;marca 2020&nbsp;r.</h3>
         <h3>Komunikat</h3>
     </div>
@@ -51,44 +73,58 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>22.&nbsp;marca 2020&nbsp;r.</h3>
-        <h3>IV NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
+        <h3>29.&nbsp;marca 2020&nbsp;r.</h3>
+        <h3>V NIEDZIELA WIELKIEGO POSTU - Rok A</h3>
     </div>
     <div class="col">
         <p>1. Dzisiaj porządek Mszy Świętych w&nbsp;naszym kościele jak
-        w&nbsp;każdą niedzielę. Zgodnie z&nbsp;zaleceniem Kurii Diecezjalnej
-        nie będzie nabożeństwa Gorzkich Zali. W&nbsp;piątek nie będzie
-        również nabożeństwa Drogi Krzyżowej. Ks.&nbsp;bp&nbsp;udzielił
-        dyspensy od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
-        do&nbsp;odwołania. Zachęca nas jednak do udziału we&nbsp;Mszy
-        Świętej i&nbsp;w&nbsp;nabożeństwach wielkopostnych korzystając
-        z&nbsp;mediów zarówno ogólnopolskich, jak i&nbsp;lokalnych.</p>
-        <p>2. Kościoły są&nbsp;otwarte, zachęcamy do&nbsp;modlitwy przed
-        Najświętszym Sakramentem o&nbsp;ustanie epidemii.</p>
-        <p>3. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
-        wspólnoty do&nbsp;wieczności Reginę Jakubczyk. <em>Wieczny
-        odpoczynek...</em></p>
-        <p>4. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
-        małżeństwa została odwołana z&nbsp;racji epidemii.</strong></p>
-        <p>5. <em>Bóg zapłać</em> Panu Ryszardowi i&nbsp;Tadeuszowi
+        w&nbsp;każdą niedzielę. Ze&nbsp;względu na&nbsp;zmianę czasu
+        na&nbsp;letni, Msza Święta wieczorna będzie dzisiaj o&nbsp;godzinie
+        20.30. Zgodnie z&nbsp;zaleceniem Kurii Diecezjalnej nie będzie
+        nabożeństwa Gorzkich Zali, w&nbsp;piątek nie będzie również
+        nabożeństwa Drogi Krzyżowej.</p>
+        <p>2. Mimo I&nbsp;piątku i&nbsp;I&nbsp;soboty, nie będzie odwiedzin
+        chorych ze&nbsp;względu na&nbsp;obowiązujące przepisy z&nbsp;racji
+        epidemii.</p>
+        <p>3. Ks.&nbsp;bp udzielił dyspensy od&nbsp;udziału we&nbsp;Mszy
+        Świętej w&nbsp;niedziele do&nbsp;odwołania. Zachęca nas jednak
+        do&nbsp;udziału we&nbsp;Mszy Świętej i&nbsp;w&nbsp;nabożeństwach
+        wielkopostnych korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
+        i&nbsp;lokalnych.</p>
+        <p>4. Od&nbsp;jutra do&nbsp;odwołania będzie w&nbsp;naszym kościele
+        w&nbsp;dni powszednie Msza Święta o&nbsp;godzinie 6.30, 7.00, 17.30
+        i&nbsp;18.00. Może uczestniczyć w&nbsp;czasie nabożeństwa tylko
+        5&nbsp;osób. Kościoły są&nbsp;otwarte przez cały dzień, zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii.</p>
+        <p>5. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
+        wspólnoty do&nbsp;wieczności Teresę Łukaszek, Józefa Sperkę
+        i&nbsp;Ryszarda Kuminek. <em>Wieczny odpoczynek...</em></p>
+        <p>6. <strong>Katechizacja przygotowująca do&nbsp;Sakramentu
+        małżeństwa dla tych, którzy planują brać ślub do&nbsp;września jest
+        prowadzona przez internet. Katechezy będą w&nbsp;piątek i&nbsp;wtorek
+        o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
+        jest w&nbsp;gablocie.</strong></p>
+        <p>7. <em>Bóg zapłać</em> Panu Ryszardowi i&nbsp;Tadeuszowi
         za&nbsp;pracę dla dobra Parafii.</p>
-        <p>6. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        <p>8. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
         na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
-        od&nbsp;21 do&nbsp;30 włącznie.</p>
-        <p>7. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        od&nbsp;31 do&nbsp;35 włącznie. (wyznaczamy mniej, bo&nbsp;może być
+        tylko 5&nbsp;osób w&nbsp;grupie).</p>
+        <p>9. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
         Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
         Zachęcamy do&nbsp;nabywania baranków wielkanocnych – Akcja
         Caritas.</p>
-        <p>8. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        <p>10. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
         innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech.
         Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
         do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>9. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>11. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
         urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
-        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej
-        modlitwie w&nbsp;waszych intencjach.</p>
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 22.&nbsp;marca 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 29.&nbsp;marca 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
