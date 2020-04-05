067c639f32f8f17edf96fb9c786768d3f75ea0de
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-04-05 21:56:02 +0100

    2020-04-05

--- right_pane.php
+++ right_pane.php
@@ -1,5 +1,41 @@
 <!-- prawy panel -->
 <div class="col-md-4 rightPanel">
+    <h3>
+        Drodzy Parafianie
+    </h3>
+    <p>
+        W&nbsp;związku z&nbsp;zaistniałą sytuacją epidemiologiczną
+        i&nbsp;zgodnie z&nbsp;przepisami, i&nbsp;zaleceniami
+        odpowiednich władz nie będzie poświęcenia pokarmów
+        w&nbsp;naszym kościele. W&nbsp;związku z&nbsp;tym
+        przekazujemy Wam formularz BŁOGOSŁAWIEŃSTWA STOŁU PRZED
+        UROCZYSTYM POSIŁKIEM W&nbsp;NIEDZIELĘ ZMARTWYCHWSTANIA
+        PAŃSKIEGO, który otrzymaliśmy od&nbsp;Sandomierskiej Kurii
+        Diecezjalnej. Skorzystajcie z&nbsp;niego w&nbsp;Waszych
+        domach.<br />
+        Pamiętajmy o&nbsp;wzajemnej modlitwie, która pomoże nam
+        przeżyć ten trudny czas w&nbsp;duchu nawrócenia
+        i&nbsp;przemiany serc.<br />
+        <br />
+        Z&nbsp;modlitewną pamięcią<br />
+        <em>Wasi Duszpasterze</em><br />
+        <br />
+        <a href="static/download/2020_wielkanoc_blogoslawienstwo_stolu.pdf">
+        <i class="fa fa-download"></i> Formularz błogosławieństwa
+        do&nbsp;pobrania</a>
+    </p>
+    <h3>
+        PRZYPOMINAMY
+    </h3>
+    <p>
+        PRZYPOMINAMY, ŻE&nbsp;ZGODNIE Z&nbsp;OBOWIĄZUJĄCYMI
+        OBOSTRZENIAMI I&nbsp;ZARZĄDZENIEM KURII DIECEZJALNEJ
+        W&nbsp;SANDOMIERZU W&nbsp;KOŚCIELE W&nbsp;CZASIE
+        ZGROMADZEŃ LITURGICZNYCH MOŻE PRZEBYWAĆ<br /><br />
+
+        MAKSYMALNIE 5&nbsp;OSÓB<br /><br />
+        PIERWSZEŃSTWO MAJĄ OSOBY, KTÓRE ZAMÓWIŁY INTENCJE MSZALNĄ.
+    </p>
     <h3 class="mt-2">
         <a href="pomoc_zywnosciowa.php">PROGRAM POMOCY ŻYWNOŚCIOWEJ 2016</a>
     </h3>
