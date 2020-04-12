f9dd7a2667cf2ab0ca4764ffc3c6e4f9feeb7489
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-04-12 08:31:22 +0100

    2020-04-12

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,91 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 29.&nbsp;marca 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 5.&nbsp;kwietnia 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>5.&nbsp;kwietnia 2020&nbsp;r.</h3>
+        <h3>NIEDZIELA PALMOWA - Rok A</h3>
+    </div>
+    <div class="col">
+        <p>1. Dzisiaj porządek Mszy Świętych w&nbsp;naszym kościele jak
+        w&nbsp;każdą niedzielę. Msza Święta wieczorna jest już odprawiana
+        o&nbsp;godzinie 20.30. Zgodnie z&nbsp;zaleceniem Kurii Diecezjalnej
+        nie będzie nabożeństwa Gorzkich Żali, w&nbsp;piątek nie będzie
+        również nabożeństwa Drogi Krzyżowej. <strong>Podczas nabożeństw
+        w&nbsp;kościele może przebywać tylko 5&nbsp;osób.</strong></p>
+        <p>2. Ks.&nbsp;bp udzielił dyspensy od&nbsp;udziału we&nbsp;Mszy
+        Świętej w&nbsp;niedziele do&nbsp;odwołania. Zachęca nas jednak
+        do&nbsp;udziału we&nbsp;Mszy Świętej i&nbsp;w&nbsp;nabożeństwach
+        wielkopostnych korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
+        i&nbsp;lokalnych.</p>
+        <p>3. Aby umożliwić wiernym udział we&nbsp;Mszy Świętej
+        (ze&nbsp;względu na&nbsp;ograniczenia) w&nbsp;poniedziałek, wtorek
+        i&nbsp;środę, Msza Święta będzie odprawiona o&nbsp;godzinie
+        <strong>6.30, 7.00, 17.30 i&nbsp;18.00</strong>. Kościoły
+        są&nbsp;otwarte przez cały dzień, zachęcamy do&nbsp;modlitwy przed
+        Najświętszym Sakramentem o&nbsp;ustanie epidemii.</p>
+        <p>4. <strong>W&nbsp;Wielki Czwartek Msza Święta Wieczerzy Pańskiej
+        o&nbsp;godzinie 18.00</strong>, następnie adoracja Pana Jezusa
+        w&nbsp;ciemnicy do&nbsp;21.00; <strong>w&nbsp;Wielki Piątek
+        nabożeństwo o&nbsp;godzinie 18.00</strong>, adoracja Krzyża przez
+        uklęknięcie lub głęboki skłon, następnie przeniesienie Najświętszego
+        Sakramentu do&nbsp;przygotowanego grobu Pana Jezusa i&nbsp;adoracja
+        do&nbsp;22.00; <strong>w&nbsp;Wielką Sobotę nie ma&nbsp;poświęcenia
+        pokarmów</strong>, jest natomiast prywatna adoracja Pana Jezusa
+        w&nbsp;Grobie Pańskim.</p>
+        <p>5. Formularz modlitwy przed rozpoczęciem i&nbsp;po&nbsp;uroczystym
+        posiłku wielkanocnym jest na&nbsp;stronie internetowej Parafii
+        i&nbsp;w&nbsp;gablocie parafialnej.</p>
+        <p>
+            <a href="static/download/2020_wielkanoc_blogoslawienstwo_stolu.pdf">
+            <i class="fa fa-download"></i> Formularz błogosławieństwa
+            do&nbsp;pobrania</a>
+        </p>
+        <p>6. <strong>Liturgia Wielkiej Soboty</strong> (okrojona,
+        ze&nbsp;względu na&nbsp;panującego koronawirusa) <strong>rozpocznie
+        się o&nbsp;godzinie 19.00</strong>.</p>
+        <p>7. <strong>Rezurekcja bez procesji w&nbsp;Niedzielę
+        Zmartwychwstania o&nbsp;godzinie 6.00. Następne Msze Święte
+        o&nbsp;godzinie 7.30, 9.30, 11.15. 12.30, 16 i&nbsp;20.30.</strong>
+        Zapraszamy.</p>
+        <p>8. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
+        wspólnoty do&nbsp;wieczności Stanisława Pietrzyk. <em>Wieczny
+        odpoczynek</em></p>
+        <p>9. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
+        tych, którzy planują brać ślub do&nbsp;września jest prowadzona
+        przez Internet. Katechezy we&nbsp;wtorek i&nbsp;piątek
+        o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
+        jest w&nbsp;gablocie.</p>
+        <p>10. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;złożoną
+        ofiarę Panom Mateuszowi i&nbsp;Witoldowi, pracę dla Parafii Panu
+        Ryszardowi i&nbsp;Tadeuszowi, <em>Bóg zapłać</em> za&nbsp;kwiaty
+        ofiarowane dla kościoła Rodzinie Judów, Zelatorkom KŻR Barbarze
+        i&nbsp;Marii za&nbsp;ofiary na&nbsp;kwiaty do&nbsp;Bożego Grobu.</p>
+        <p>11. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
+        od&nbsp;36 do&nbsp;40 włącznie. (wyznaczamy mniej, bo&nbsp;może być
+        tylko 5&nbsp;osób w&nbsp;grupie).</p>
+        <p>12. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
+        Zachęcamy do&nbsp;nabywania baranków wielkanocnych – Akcja
+        Caritas.</p>
+        <p>13. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech.
+        Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
+        do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>14. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 5.&nbsp;kwietnia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>29.&nbsp;marca 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,5 +1,72 @@
 <div class="card">
     <div class="card-header">
+        <h3>12.&nbsp;kwietnia 2020&nbsp;r.</h3>
+        <h3>NIEDZIELA Zmartwychwstania Pańskiego - Rok A</h3>
+    </div>
+    <div class="col">
+        <p>1. <strong>Kochani Moi Parafianie i&nbsp;Goście. Do&nbsp;życzeń
+        otrzymywanych od&nbsp;Rodziny, Przyjaciół i&nbsp;Znajomych chcemy
+        dołączyć życzenia od&nbsp;nas kapłanów. Życzymy Wam z&nbsp;całego
+        serca umocnienia wiary w&nbsp;Zmartwychwstanie Jezusa, wzrastania
+        w&nbsp;miłości ku&nbsp;Bogu, który nas kocha, doskonalenia
+        w&nbsp;sobie miłości do&nbsp;bliźnich, którzy nas otaczają oraz
+        ożywiania nadziei, że&nbsp;lęk i&nbsp;przygnębienie, które
+        przeżywamy z&nbsp;racji trwającej na&nbsp;całym globie epidemii
+        koronawirusa minie i&nbsp;znowu będziemy mogli cieszyć się pełną
+        swobodą obywatelską w&nbsp;naszej Ojczyźnie. Zmartwychwstały Jezus
+        pokonał przecież szatana, grzech i&nbsp;wszelkie lęki. Wyszedł
+        z&nbsp;grobu i&nbsp;mówi wciąż do&nbsp;nas: nie lękajcie się.
+        Zdrowych, radosnych i&nbsp;błogosławionych Świąt Zmartwychwstania
+        Pańskiego.</strong></p>
+        <p>2. Dzisiaj i&nbsp;jutro porządek Mszy Świętych w&nbsp;naszym
+        kościele jak w&nbsp;każdą niedzielę, także o&nbsp;godzinie 20.30.
+        <strong>Podczas nabożeństw w&nbsp;kościele może przebywać tylko
+        5&nbsp;osób.</strong></p>
+        <p>3. Przypominamy, że&nbsp;Ks.&nbsp;bp udzielił dyspensy
+        od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
+        do&nbsp;odwołania. Zachęca nas jednak do&nbsp;udziału we&nbsp;Mszy
+        Świętej korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
+        i&nbsp;lokalnych.</p>
+        <p>4. Aby umożliwić wiernym udział we&nbsp;Mszy Świętej
+        (ze&nbsp;względu na&nbsp;ograniczenia) w&nbsp;dni powszednie Msza
+        Święta będzie odprawiona o&nbsp;godzinie <strong>6.30, 7.00, 17.30
+        i&nbsp;18.00</strong>. Kościoły są&nbsp;otwarte przez cały dzień,
+        zachęcamy do&nbsp;modlitwy przed Najświętszym Sakramentem
+        o&nbsp;ustanie epidemii.</p>
+        <p>5. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
+        tych, którzy planują brać ślub do&nbsp;września jest prowadzona przez
+        Internet. Katechezy we&nbsp;wtorek i&nbsp;piątek o&nbsp;godzinie
+        20.00. Informacja szczegółowa dla zainteresowanych jest
+        w&nbsp;gablocie.</p>
+        <p>6. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;złożoną
+        ofiarę Panom Michałowi i&nbsp;Sławomirowi, za&nbsp;przygotowanie
+        Ciemnicy i&nbsp;Grobu Pańskiego Pani Monice oraz Panom: Ryszardowi,
+        Tadeuszowi i&nbsp;Mateuszowi. <em>Bóg zapłać</em> za&nbsp;kwiaty
+        ofiarowane do&nbsp;kościoła Rodzinie Krzysztofa i&nbsp;Beaty.</p>
+        <p>7. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
+        od&nbsp;41 do&nbsp;45 włącznie. (wyznaczamy mniej, bo&nbsp;może być
+        tylko 5&nbsp;osób w&nbsp;grupie).</p>
+        <p>8. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>9. Trwają zapisy na pielgrzymkę do&nbsp;Medjugorie oraz innych
+        miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech. Bliższe
+        informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
+        do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 12.&nbsp;kwietnia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
+<div class="card">
+    <div class="card-header">
         <h3>
             Drodzy Parafianie
         </h3>
@@ -26,90 +93,6 @@
             <i class="fa fa-download"></i> Formularz błogosławieństwa
             do&nbsp;pobrania</a>
         </h4>
-        <p>
-            <span style="float: left;">Sandomierz, 5.&nbsp;kwietnia 2020&nbsp;r.</span>
-            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
-        </p>
-    </div>
-</div>
-<br />
-
-<div class="card">
-    <div class="card-header">
-        <h3>5.&nbsp;kwietnia 2020&nbsp;r.</h3>
-        <h3>NIEDZIELA PALMOWA - Rok A</h3>
-    </div>
-    <div class="col">
-        <p>1. Dzisiaj porządek Mszy Świętych w&nbsp;naszym kościele jak
-        w&nbsp;każdą niedzielę. Msza Święta wieczorna jest już odprawiana
-        o&nbsp;godzinie 20.30. Zgodnie z&nbsp;zaleceniem Kurii Diecezjalnej
-        nie będzie nabożeństwa Gorzkich Żali, w&nbsp;piątek nie będzie
-        również nabożeństwa Drogi Krzyżowej. <strong>Podczas nabożeństw
-        w&nbsp;kościele może przebywać tylko 5&nbsp;osób.</strong></p>
-        <p>2. Ks.&nbsp;bp udzielił dyspensy od&nbsp;udziału we&nbsp;Mszy
-        Świętej w&nbsp;niedziele do&nbsp;odwołania. Zachęca nas jednak
-        do&nbsp;udziału we&nbsp;Mszy Świętej i&nbsp;w&nbsp;nabożeństwach
-        wielkopostnych korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
-        i&nbsp;lokalnych.</p>
-        <p>3. Aby umożliwić wiernym udział we&nbsp;Mszy Świętej
-        (ze&nbsp;względu na&nbsp;ograniczenia) w&nbsp;poniedziałek, wtorek
-        i&nbsp;środę, Msza Święta będzie odprawiona o&nbsp;godzinie
-        <strong>6.30, 7.00, 17.30 i&nbsp;18.00</strong>. Kościoły
-        są&nbsp;otwarte przez cały dzień, zachęcamy do&nbsp;modlitwy przed
-        Najświętszym Sakramentem o&nbsp;ustanie epidemii.</p>
-        <p>4. <strong>W&nbsp;Wielki Czwartek Msza Święta Wieczerzy Pańskiej
-        o&nbsp;godzinie 18.00</strong>, następnie adoracja Pana Jezusa
-        w&nbsp;ciemnicy do&nbsp;21.00; <strong>w&nbsp;Wielki Piątek
-        nabożeństwo o&nbsp;godzinie 18.00</strong>, adoracja Krzyża przez
-        uklęknięcie lub głęboki skłon, następnie przeniesienie Najświętszego
-        Sakramentu do&nbsp;przygotowanego grobu Pana Jezusa i&nbsp;adoracja
-        do&nbsp;22.00; <strong>w&nbsp;Wielką Sobotę nie ma&nbsp;poświęcenia
-        pokarmów</strong>, jest natomiast prywatna adoracja Pana Jezusa
-        w&nbsp;Grobie Pańskim.</p>
-        <p>5. Formularz modlitwy przed rozpoczęciem i&nbsp;po&nbsp;uroczystym
-        posiłku wielkanocnym jest na&nbsp;stronie internetowej Parafii
-        i&nbsp;w&nbsp;gablocie parafialnej.</p>
-        <p>
-            <a href="static/download/2020_wielkanoc_blogoslawienstwo_stolu.pdf">
-            <i class="fa fa-download"></i> Formularz błogosławieństwa
-            do&nbsp;pobrania</a>
-        </p>
-        <p>6. <strong>Liturgia Wielkiej Soboty</strong> (okrojona,
-        ze&nbsp;względu na&nbsp;panującego koronawirusa) <strong>rozpocznie
-        się o&nbsp;godzinie 19.00</strong>.</p>
-        <p>7. <strong>Rezurekcja bez procesji w&nbsp;Niedzielę
-        Zmartwychwstania o&nbsp;godzinie 6.00. Następne Msze Święte
-        o&nbsp;godzinie 7.30, 9.30, 11.15. 12.30, 16 i&nbsp;20.30.</strong>
-        Zapraszamy.</p>
-        <p>8. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
-        wspólnoty do&nbsp;wieczności Stanisława Pietrzyk. <em>Wieczny
-        odpoczynek</em></p>
-        <p>9. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
-        tych, którzy planują brać ślub do&nbsp;września jest prowadzona
-        przez Internet. Katechezy we&nbsp;wtorek i&nbsp;piątek
-        o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
-        jest w&nbsp;gablocie.</p>
-        <p>10. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;złożoną
-        ofiarę Panom Mateuszowi i&nbsp;Witoldowi, pracę dla Parafii Panu
-        Ryszardowi i&nbsp;Tadeuszowi, <em>Bóg zapłać</em> za&nbsp;kwiaty
-        ofiarowane dla kościoła Rodzinie Judów, Zelatorkom KŻR Barbarze
-        i&nbsp;Marii za&nbsp;ofiary na&nbsp;kwiaty do&nbsp;Bożego Grobu.</p>
-        <p>11. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
-        od&nbsp;36 do&nbsp;40 włącznie. (wyznaczamy mniej, bo&nbsp;może być
-        tylko 5&nbsp;osób w&nbsp;grupie).</p>
-        <p>12. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
-        Zachęcamy do&nbsp;nabywania baranków wielkanocnych – Akcja
-        Caritas.</p>
-        <p>13. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
-        innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech.
-        Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
-        do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>14. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
-        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
-        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
-        w&nbsp;waszych intencjach.</p>
         <p>
             <span style="float: left;">Sandomierz, 5.&nbsp;kwietnia 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
