df6d8939909bb4cbd19d8fd10fd45a6410472603
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-04-26 09:44:41 +0100

    2020-04-26

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,60 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 12.&nbsp;kwietnia 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 19.&nbsp;kwietnia 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>12.&nbsp;kwietnia 2020&nbsp;r.</h3>
+        <h3>NIEDZIELA  Miłosierdzia Bożego - Rok A</h3>
+        <h3>II niedziela wielkanocna</h3>
+    </div>
+    <div class="col">
+        <p>1. <strong>Jak wiemy od&nbsp;jutra w&nbsp;kościele może być
+        na&nbsp;Mszy Świętej więcej osób</strong>: 1&nbsp;osoba
+        na&nbsp;15&nbsp;m<sup>2</sup>. U&nbsp;nas w&nbsp;kościele może
+        uczestniczyć 47&nbsp;osób. Zapraszamy zatem korzystajcie
+        z&nbsp;tej możliwości. Od&nbsp;jutra w&nbsp;dni powszednie
+        Msza Święta będzie odprawiana o&nbsp;6.30 i&nbsp;18.00.</p>
+        <p>2. Przypominamy, że&nbsp;Ks.&nbsp;bp&nbsp;udzielił dyspensy
+        od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
+        do&nbsp;odwołania. Zachęca nas jednak do&nbsp;udziału we&nbsp;Mszy
+        Świętej korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
+        i&nbsp;lokalnych.</p>
+        <p>3. Kościół jest otwarty przez cały dzień, zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii.</p>
+        <p>4. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej Parafii
+        do&nbsp;wieczności: Czesławę Lipiec, Andrzeja Walicha, Teodozję
+        Marczyk i&nbsp;Mieczysława Osmalę. Polećmy ich Bożemu miłosierdziu
+        odmawiając: <em>Wieczny odpoczynek...</em></p>
+        <p>5. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
+        tych, którzy planują brać ślub do&nbsp;września jest prowadzona przez
+        Internet. Katechezy są&nbsp;we&nbsp;wtorek i&nbsp;piątek
+        o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
+        jest w&nbsp;gablocie.</p>
+        <p>6. <em>Bóg zapłać</em> za&nbsp;prace dla dobra parafii Panom:
+        Tadeuszowi i&nbsp;Ryszardowi.</p>
+        <p>7. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
+        od&nbsp;46 do&nbsp;50 włącznie.</p>
+        <p>8. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>9. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
+        innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech.
+        Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
+        do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
+        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 19.&nbsp;kwietnia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>12.&nbsp;kwietnia 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,50 +1,62 @@
 <div class="card">
     <div class="card-header">
-        <h3>12.&nbsp;kwietnia 2020&nbsp;r.</h3>
-        <h3>NIEDZIELA  Miłosierdzia Bożego - Rok A</h3>
-        <h3>II niedziela wielkanocna</h3>
+        <h3>26.&nbsp;kwietnia 2020&nbsp;r.</h3>
+        <h3>III NIEDZIELA WIELKANOCNA - Rok A</h3>
     </div>
     <div class="col">
-        <p>1. <strong>Jak wiemy od&nbsp;jutra w&nbsp;kościele może być
-        na&nbsp;Mszy Świętej więcej osób</strong>. 1&nbsp;osoba
-        na&nbsp;15&nbsp;m<sup>2</sup>. U&nbsp;nas w&nbsp;kościele może
-        uczestniczyć 47&nbsp;osób. Zapraszamy zatem korzystajcie
-        z&nbsp;tej możliwości. Od&nbsp;jutra w&nbsp;dni powszednie
-        Msza Święta będzie odprawiana o&nbsp;6.30 i&nbsp;18.00.</p>
-        <p>2. Przypominamy, że&nbsp;Ks.&nbsp;bp&nbsp;udzielił dyspensy
+        <p>1. <strong>Jak wiemy od&nbsp;poniedziałku 20&nbsp;kwietnia w&nbsp;kościele
+        może być na&nbsp;Mszy Świętej więcej osób: 1&nbsp;osoba
+        na&nbsp;15 m<sup>2</sup>.</strong> U&nbsp;nas w&nbsp;kościele może
+        uczestniczyć 47&nbsp;osób – oczywiście w&nbsp;rozproszeniu
+        po&nbsp;całym kościele. <strong>Zapraszamy zatem korzystajcie z&nbsp;tej
+        możliwości.</strong> Gdyby było już więcej osób, pozostajemy na&nbsp;zewnątrz
+        kościoła w&nbsp;odstępach od&nbsp;siebie – ok.&nbsp;2&nbsp;m. Będzie
+        włączone nagłośnienie, aby uczestnicy wszystko słyszeli;
+        z&nbsp;komunią Świętą kapłan będzie stał na&nbsp;podeście przy
+        drzwiach głównych.</p>
+        <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
+        i&nbsp;18.00.</p>
+        <p>3. Przypominamy, że&nbsp;Ks.&nbsp;bp udzielił dyspensy
         od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
         do&nbsp;odwołania. Zachęca nas jednak do&nbsp;udziału we&nbsp;Mszy
         Świętej korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
         i&nbsp;lokalnych.</p>
-        <p>3. Kościół jest otwarty przez cały dzień, zachęcamy
+        <p>4. Kościół jest otwarty przez cały dzień, zachęcamy
         do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
         epidemii.</p>
-        <p>4. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej Parafii
-        do&nbsp;wieczności: Czesławę Lipiec, Andrzeja Walicha, Teodozję
-        Marczyk i&nbsp;Mieczysława Osmalę. Polećmy ich Bożemu miłosierdziu
-        odmawiając: <em>Wieczny odpoczynek...</em></p>
-        <p>5. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
+        <p>5. W&nbsp;tym tygodniu jest I&nbsp;piątek i&nbsp;sobota miesiąca
+        maja. Nie idziemy z&nbsp;posługą sakramentalną do&nbsp;chorych
+        w&nbsp;domach – z&nbsp;racji korona wirusa.</p>
+        <p>6. W&nbsp;piątek 1&nbsp;maja nabożeństwo maryjne o&nbsp;godzinie
+        17.30 – i&nbsp;tak będzie w&nbsp;dni powszednie przez cały miesiąc.
+        W&nbsp;niedziele majowe będziemy śpiewać litanię po&nbsp;Mszy Świętej
+        odprawianej o&nbsp;godzinie 16.00.</p>
+        <p>7. <strong>Uroczystość Matki Bożej Królowej Polski z&nbsp;3&nbsp;maja jest
+        przeniesiona liturgicznie na&nbsp;sobotę 2&nbsp;maja, z&nbsp;tej
+        racji, że&nbsp;niedziele wielkanocne mają wyższą rangę niż
+        Uroczystości Matki Bożej i&nbsp;Świętych Pańskich. Zapraszamy
+        na&nbsp;Mszę&nbsp;Św. o&nbsp;Matce Bożej Królowej Polski
+        w&nbsp;sobotę, która będzie odprawiana 6.30, 9.00 i&nbsp;18.00.</strong></p>
+        <p>8. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
         tych, którzy planują brać ślub do&nbsp;września jest prowadzona przez
         Internet. Katechezy są&nbsp;we&nbsp;wtorek i&nbsp;piątek
         o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
         jest w&nbsp;gablocie.</p>
-        <p>6. <em>Bóg zapłać</em> za&nbsp;prace dla dobra parafii Panom:
-        Tadeuszowi i&nbsp;Ryszardowi.</p>
-        <p>7. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
-        od&nbsp;46 do&nbsp;50 włącznie.</p>
-        <p>8. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        <p>9. <em>Bóg zapłać</em> za&nbsp;prace dla dobra parafii Panom:
+        Tadeuszowi i&nbsp;Ryszardowi. <em>Bóg zapłać</em> za&nbsp;ofiary,
+        przesyłacie na&nbsp;konto bankowe lub składacie w&nbsp;zakrystii.</p>
+        <p>10. <strong>Do&nbsp;sprzątania prosimy w&nbsp;piątek
+        po&nbsp;Mszy&nbsp;św. wieczornej, o&nbsp;godz.&nbsp;18.30,</strong>
+        Rodziny z&nbsp;bloku Maciejowskiego 6 od&nbsp;50 do&nbsp;55
+        włącznie.</p>
+        <p>11. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
         Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>9. Trwają zapisy na&nbsp;pielgrzymkę do&nbsp;Medjugorie oraz
-        innych miejsc w&nbsp;Bośni i&nbsp;Hercegowinie, Chorwacji, Węgrzech.
-        Bliższe informacje na&nbsp;plakacie w&nbsp;gablocie przy wejściu
-        do&nbsp;kościoła oraz w&nbsp;zakrystii.</p>
-        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>12. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
         urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
         składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
         w&nbsp;waszych intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 19.&nbsp;kwietnia 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 26.&nbsp;kwietnia 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
