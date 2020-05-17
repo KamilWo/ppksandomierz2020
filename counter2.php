bc5000bfe2a7a0b178c619631687d9738658e846
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-05-17 21:49:24 +0100

    New counter initial commit

---
+++ counter2.php
@@ -0,0 +1,18 @@
+
+<p style="text-align: right;"><br/>Od 31. marca 2012 r.<br/>stronę odwiedziło osób:<br/><br/>
+<span>
+<?php
+
+$visits_number = "00098439";
+for ($d = 0; $d < strlen($visits_number); $d++){
+    echo "<img src=\"static/src/images/counter/" . $visits_number[$d] . ".png\" alt=\"" .
+    $visits_number[$d] . "\" style=\"border-width: 0px;\"/>\n";
+}
+
+?>
+</span>
+<?php
+//echo $_SERVER['REMOTE_ADDR'];
+?>
+<!--<script type="text/javascript" src="http://www.darmowe-liczniki.pl/usluga.licznik.24821"></script>-->
+</p>
