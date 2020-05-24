f1b16f861d1fb42ffbc1f48f1478d13c47137eeb
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-05-24 11:50:10 +0100

    2020-05-24

--- last_post.php
+++ last_post.php
@@ -1,23 +1,24 @@
 <div class="card">
     <div class="card-header">
-        <h3>17.&nbsp;maja 2020&nbsp;r.</h3>
-        <h3>VI NIEDZIELA WIELKANOCNA - Rok A</h3>
+        <h3>24.&nbsp;maja 2020&nbsp;r.</h3>
+        <h3>VII NIEDZIELA WIELKANOCNA - Rok A</h3>
+        <h3>Uroczystość Wniebowstąpienia Pańskiego</h3>
     </div>
     <div class="col">
-        <p>1. <strong>Jak wiemy, od&nbsp;dzisiaj - 17&nbsp;maja -
+        <p>1. <strong>Jak wiemy, od&nbsp;17&nbsp;maja -
         w&nbsp;kościele może być na&nbsp;Mszy Świętej więcej osób.</strong>
-        1&nbsp;osoba na&nbsp;10&nbsp;m<sup>2</sup>. U&nbsp;nas
-        w&nbsp;kościele może uczestniczyć <strong>72&nbsp;osoby</strong>
-        – oczywiście w&nbsp;rozproszeniu po&nbsp;całym kościele.
-        <strong>Zapraszamy zatem korzystajcie z&nbsp;tej możliwości.</strong>
-        Gdyby było już więcej osób, pozostańmy na&nbsp;zewnątrz kościoła
-        w&nbsp;odstępach od&nbsp;siebie – ok.&nbsp;2&nbsp;m. Będzie włączone
-        nagłośnienie, aby uczestnicy wszystko słyszeli; z&nbsp;komunią Świętą
-        kapłan będzie stał na&nbsp;podeście przy drzwiach głównych.</p>
+        1&nbsp;osoba na&nbsp;10 m<sup>2</sup>. U&nbsp;nas w&nbsp;kościele
+        może uczestniczyć <strong>72&nbsp;osoby</strong> – oczywiście
+        w&nbsp;rozproszeniu po&nbsp;całej świątyni. <strong>Zapraszamy
+        zatem korzystajcie z&nbsp;tej możliwości.</strong> Gdyby było już
+        więcej osób, pozostańmy na&nbsp;zewnątrz kościoła w&nbsp;odstępach
+        od&nbsp;siebie – ok.&nbsp;2&nbsp;m. Będzie włączone nagłośnienie,
+        aby uczestnicy wszystko słyszeli; z&nbsp;komunią Świętą kapłan
+        będzie stał na&nbsp;podeście przy drzwiach głównych.</p>
         <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
         i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia w&nbsp;tym
         okresie niech sprawdza w&nbsp;gablocie na&nbsp;wywieszce.</p>
-        <p>3. Przypominamy, że&nbsp;Ks.&nbsp;bp udzielił dyspensy
+        <p>3. Przypominamy, że&nbsp;Ks.&nbsp;bp&nbsp;udzielił dyspensy
         od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
         do&nbsp;odwołania. Zachęca nas jednak do&nbsp;udziału we&nbsp;Mszy
         Świętej korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
@@ -32,35 +33,40 @@
         przez Internet. Katechezy są&nbsp;we&nbsp;wtorek i&nbsp;piątek
         o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
         jest w&nbsp;gablocie.</p>
-        <p>7. <em>Bóg zapłać</em> za&nbsp;złożoną ofiarę na&nbsp;sprzątanie
-        Pani Ani i&nbsp;za&nbsp;pracę dla dobra parafii Panom: Adamowi,
-        Ryszardowii Tadeuszowi. <em>Bóg zapłać</em> za&nbsp;ofiary dla
-        Parafii, które przesyłacie na&nbsp;konto bankowe lub składacie
-        w&nbsp;zakrystii.</p>
-        <p>8. Informujemy, że&nbsp;Jadłodajnia Romana od&nbsp;jutra -
-        18&nbsp;maja - będzie pracować jak dawniej, tzn. z&nbsp;możliwością
-        konsumpcji posiłków na&nbsp;miejscu. Jeżeli będzie taka potrzeba,
-        będzie możliwość spożywania posiłków na&nbsp;dużej sali, aby zachować
-        odpowiednie odległości między stolikami. Zapraszamy.</p>
-        <p>9. Pielgrzymka do&nbsp;Medjugorie została odwołana z&nbsp;racji
+        <p>7. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
+        wspólnoty do&nbsp;wieczności Teresę Świątańską. Polećmy
+        ją&nbsp;Bożemu miłosierdziu odmawiając: <em>ieczny odpoczynek...</em></p>
+        <p>8. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;złożoną
+        ofiarę na&nbsp;sprzątanie oraz na&nbsp;kwiaty, za&nbsp;pracę dla
+        dobra parafii Panom: Tadeuszowi i&nbsp;Ryszardowi, za&nbsp;podlewanie
+        kwiatów na&nbsp;rabatkach Rodzinie Daniela i&nbsp;Agaty wraz
+        z&nbsp;dziećmi: Gabrysiem i&nbsp;Antosiem. <em>Bóg zapłać</em>
+        za&nbsp;ofiary dla Parafii, które przesyłacie na&nbsp;konto bankowe
+        lub składacie w&nbsp;zakrystii.</p>
+        <p>9. Informujemy, że&nbsp;Jadłodajnia Romana już pracuje, jak
+        dawniej, tzn. z&nbsp;możliwością konsumpcji posiłków na&nbsp;miejscu.
+        Jeżeli będzie taka potrzeba, będzie możliwość spożywania posiłków
+        na&nbsp;dużej sali, aby zachować odpowiednie odległości między
+        stolikami. Zapraszamy.</p>
+        <p>10. Pielgrzymka do&nbsp;Medjugorie została odwołana z&nbsp;racji
         koronawirusa. Ks.&nbsp;Krzysztof będzie się kontaktował telefonicznie
         z&nbsp;zapisanymi osobami.</p>
-        <p>10. Bardzo prosimy poruszających się po&nbsp;placu kościelnym
+        <p>11. Bardzo prosimy poruszających się po&nbsp;placu kościelnym
         samochodami, aby nie wjeżdżać na&nbsp;rabatki kwiatowe i&nbsp;nie
         niszczyć kwiatów. Pomyśl sobie, że&nbsp;Ciebie ktoś przejeżdża
         samochodem, jak byś się czuł - podobnie cierpią kwiaty przejechane
         przez kierowcę.</p>
-        <p>11. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 6
-        od&nbsp;66 do&nbsp;70 włącznie.</p>
-        <p>12. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        <p>12. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 5
+        od&nbsp;1 do&nbsp;10 włącznie.</p>
+        <p>13. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
         Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>14. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
         urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
         składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
         w&nbsp;waszych intencjach.</p>
         <p>
-            <span style="float: left;">Sandomierz, 17.&nbsp;maja 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 24.&nbsp;maja 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
