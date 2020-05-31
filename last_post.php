c1b96ce249e4fbb68075284793a4c3c4b7d68357
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-05-31 10:13:15 +0100

    2020-05-31

--- last_post.php
+++ last_post.php
@@ -76,6 +76,74 @@
 <div class="card">
     <div class="card-header">
         <h3>
+            Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy Świętej
+        </h3>
+    </div>
+    <div class="col">
+        <p>
+            29&nbsp;maja 2020&nbsp;r. Biskup Sandomierski Krzysztof
+            Nitkiewicz wydał dekret o&nbsp;tym, że&nbsp;dyspensa
+            od&nbsp;obowiązku uczestnictwa we&nbsp;Mszy&nbsp;św.
+            w&nbsp;niedziele i&nbsp;dni nakazane, udzielona 12&nbsp;marca
+            br. i&nbsp;przedłużona 21&nbsp;marca, obowiązuje jedynie
+            <strong>do&nbsp;dnia 31&nbsp;maja br. włącznie</strong>.
+        </p>
+        <p style="text-align: center;"><strong>DEKRET</strong><br>
+            <strong>o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy
+            św.</strong>
+        </p>
+        <p>W&nbsp;Najświętszej Eucharystii, która&nbsp;urzeczywistnia
+        Komunię z&nbsp;Bogiem i&nbsp;jedność Ludu Bożego jest obecny
+        sam Chrystus, nasza Pascha i&nbsp;Chleb Żywy (por. PO&nbsp;5,
+        KKK 1325).</p>
+        <p>Dlatego przyjmując z&nbsp;radością zniesienie ograniczeń
+        sanitarnych dotyczących liczby uczestników liturgii
+        odprawianej wewnątrz świątyni, postanawiam, że&nbsp;dyspensa
+        od&nbsp;obowiązku uczestnictwa we&nbsp;Mszy
+        św.&nbsp;w&nbsp;niedziele i&nbsp;dni nakazane, udzielona
+        12&nbsp;marca br.&nbsp;i&nbsp;przedłużona 21 marca,
+        obowiązuje jedynie do&nbsp;dnia 31 maja br.&nbsp;włącznie.</p>
+        <p>Jednocześnie udzielam dyspensy od&nbsp;obowiązku
+        uczestnictwa we&nbsp;Mszy św.&nbsp;w&nbsp;niedziele
+        i&nbsp;dni nakazane osobom: w&nbsp;podeszłym wieku, mającym
+        objawy infekcji (np.&nbsp;podwyższona temperatura, kaszel,
+        katar itd.), przebywającym na&nbsp;kwarantannie i&nbsp;tym,
+        które z&nbsp;innych powodów są szczególnie podatne
+        na&nbsp;zakażenie bądź obawiając się jego, unikają
+        przebywania w&nbsp;miejscach publicznych. Dyspensa
+        ta&nbsp;będzie obowiązywała do&nbsp;końca czerwca br.</p>
+        <p>Wierni uczestniczący w&nbsp;liturgii mają obowiązek
+        przestrzegania wszystkich przepisów sanitarnych wydanych
+        przez&nbsp;władze państwowe.</p>
+        <p>Księża Proboszczowie i&nbsp;Rektorzy kościołów dołożą
+        wszelkich starań, aby zasady te zostały zachowane. Powinni
+        jednocześnie zachęcić wiernych do&nbsp;pełnego uczestnictwa
+        we&nbsp;Mszy św., organizując – z&nbsp;zachowaniem
+        obowiązujących przepisów sanitarnych – adoracje
+        oraz&nbsp;inne formy kultu Jezusa Eucharystycznego.</p>
+        <p>&nbsp;</p>
+        <p style="text-align: right;"><em>Contrariis quibuslibet minime obstantibus.</em></p>
+        <p style="text-align: right;">Sandomierz, 29 maja 2020 r.</p>
+        <p>&nbsp;</p>
+        <p style="text-align: right;">Biskup Sandomierski<br>
+        Krzysztof Nitkiewicz</p>
+        <p style="text-align: right;">Kanclerz<br>
+        ks. Roman Janiec</p>
+        <p><a href="https://diecezjasandomierska.pl/wp-content/uploads/2020/05/Dekret-o-obowiązku-uczestnictwa-we-Mszy-św..pdf">Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy św.</a></p>
+
+        <p>
+            <a href="https://diecezjasandomierska.pl/dekret-o-obowiazku-uczestnictwa-we-mszy-sw/">
+            Link do dekretu na stronie Diecezji Sandomierskiej</a><br />
+            <a href="static/download/Dekret-o-obowiazku-uczestnictwa-we-Mszy-sw_2020-05-29.pdf">
+            Plik do pobrania z naszego serwera</a>
+        </p>
+    </div>
+</div>
+<br />
+
+<div class="card">
+    <div class="card-header">
+        <h3>
             Zwiększono dopuszczalną liczbę osób
         </h3>
     </div>
--- right_pane.php
+++ right_pane.php
@@ -1,6 +1,23 @@
 <!-- prawy panel -->
 <div class="col-md-4 rightPanel">
     <h3>
+        Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy Świętej
+    </h3>
+    <p>
+        29&nbsp;maja 2020&nbsp;r. Biskup Sandomierski Krzysztof
+        Nitkiewicz wydał dekret o&nbsp;tym, że&nbsp;dyspensa
+        od&nbsp;obowiązku uczestnictwa we&nbsp;Mszy&nbsp;św.
+        w&nbsp;niedziele i&nbsp;dni nakazane, udzielona 12&nbsp;marca
+        br. i&nbsp;przedłużona 21&nbsp;marca, obowiązuje jedynie
+        <strong>do&nbsp;dnia 31&nbsp;maja br. włącznie</strong>.
+    </p>
+    <p>
+        <a href="https://diecezjasandomierska.pl/dekret-o-obowiazku-uczestnictwa-we-mszy-sw/">
+        Link do dekretu na stronie Diecezji Sandomierskiej</a><br />
+        <a href="static/download/Dekret-o-obowiazku-uczestnictwa-we-Mszy-sw_2020-05-29.pdf">
+        Plik do pobrania</a>
+    </p>
+    <h3>
         Zwiększono dopuszczalną liczbę osób
     </h3>
     <p>
