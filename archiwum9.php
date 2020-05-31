0d5a50848a0244ed8653c0d76ff184021254fced
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-05-31 10:39:55 +0100

    2020-05-31

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,82 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 17.&nbsp;maja 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 24.&nbsp;maja 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>24.&nbsp;maja 2020&nbsp;r.</h3>
+        <h3>VII NIEDZIELA WIELKANOCNA - Rok A</h3>
+        <h3>Uroczystość Wniebowstąpienia Pańskiego</h3>
+    </div>
+    <div class="col">
+        <p>1. <strong>Jak wiemy, od&nbsp;17&nbsp;maja -
+        w&nbsp;kościele może być na&nbsp;Mszy Świętej więcej osób.</strong>
+        1&nbsp;osoba na&nbsp;10 m<sup>2</sup>. U&nbsp;nas w&nbsp;kościele
+        może uczestniczyć <strong>72&nbsp;osoby</strong> – oczywiście
+        w&nbsp;rozproszeniu po&nbsp;całej świątyni. <strong>Zapraszamy
+        zatem korzystajcie z&nbsp;tej możliwości.</strong> Gdyby było już
+        więcej osób, pozostańmy na&nbsp;zewnątrz kościoła w&nbsp;odstępach
+        od&nbsp;siebie – ok.&nbsp;2&nbsp;m. Będzie włączone nagłośnienie,
+        aby uczestnicy wszystko słyszeli; z&nbsp;komunią Świętą kapłan
+        będzie stał na&nbsp;podeście przy drzwiach głównych.</p>
+        <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
+        i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia w&nbsp;tym
+        okresie niech sprawdza w&nbsp;gablocie na&nbsp;wywieszce.</p>
+        <p>3. Przypominamy, że&nbsp;Ks.&nbsp;bp&nbsp;udzielił dyspensy
+        od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
+        do&nbsp;odwołania. Zachęca nas jednak do&nbsp;udziału we&nbsp;Mszy
+        Świętej korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
+        i&nbsp;lokalnych.</p>
+        <p>4. Kościół jest otwarty przez cały dzień, zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii.</p>
+        <p>5. Nabożeństwo majowe w&nbsp;dni powszednie odprawiamy
+        o&nbsp;godzinie 17.30.</p>
+        <p>6. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
+        tych, którzy planują brać ślub do&nbsp;września jest prowadzona
+        przez Internet. Katechezy są&nbsp;we&nbsp;wtorek i&nbsp;piątek
+        o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
+        jest w&nbsp;gablocie.</p>
+        <p>7. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
+        wspólnoty do&nbsp;wieczności Teresę Świątańską. Polećmy
+        ją&nbsp;Bożemu miłosierdziu odmawiając: <em>Wieczny odpoczynek...</em></p>
+        <p>8. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;złożoną
+        ofiarę na&nbsp;sprzątanie oraz na&nbsp;kwiaty, za&nbsp;pracę dla
+        dobra parafii Panom: Tadeuszowi i&nbsp;Ryszardowi, za&nbsp;podlewanie
+        kwiatów na&nbsp;rabatkach Rodzinie Daniela i&nbsp;Agaty wraz
+        z&nbsp;dziećmi: Gabrysiem i&nbsp;Antosiem. <em>Bóg zapłać</em>
+        za&nbsp;ofiary dla Parafii, które przesyłacie na&nbsp;konto bankowe
+        lub składacie w&nbsp;zakrystii.</p>
+        <p>9. Informujemy, że&nbsp;Jadłodajnia Romana już pracuje, jak
+        dawniej, tzn. z&nbsp;możliwością konsumpcji posiłków na&nbsp;miejscu.
+        Jeżeli będzie taka potrzeba, będzie możliwość spożywania posiłków
+        na&nbsp;dużej sali, aby zachować odpowiednie odległości między
+        stolikami. Zapraszamy.</p>
+        <p>10. Pielgrzymka do&nbsp;Medjugorie została odwołana z&nbsp;racji
+        koronawirusa. Ks.&nbsp;Krzysztof będzie się kontaktował telefonicznie
+        z&nbsp;zapisanymi osobami.</p>
+        <p>11. Bardzo prosimy poruszających się po&nbsp;placu kościelnym
+        samochodami, aby nie wjeżdżać na&nbsp;rabatki kwiatowe i&nbsp;nie
+        niszczyć kwiatów. Pomyśl sobie, że&nbsp;Ciebie ktoś przejeżdża
+        samochodem, jak byś się czuł - podobnie cierpią kwiaty przejechane
+        przez kierowcę.</p>
+        <p>12. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 5
+        od&nbsp;1 do&nbsp;10 włącznie.</p>
+        <p>13. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>14. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 24.&nbsp;maja 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>17.&nbsp;maja 2020&nbsp;r.</h3>
@@ -75,6 +150,26 @@ include 'head.php';
         urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
         składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
         w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 17.&nbsp;maja 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
+<div class="card">
+    <div class="card-header">
+        <h3>
+            Zwiększono dopuszczalną liczbę osób
+        </h3>
+    </div>
+    <div class="col">
+        <h4>
+            Informujemy, że&nbsp;zgodnie z&nbsp;nowymi przepisami
+            na&nbsp;Mszach św. w&nbsp;naszym kościele może uczestniczyć
+            maksymalnie <strong>72&nbsp;osoby.</strong>
+        </h4>
         <p>
             <span style="float: left;">Sandomierz, 17.&nbsp;maja 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
@@ -684,7 +779,8 @@ include 'head.php';
     </div>
     <div class="col">
         <p>Nowe zarządzenie z dnia 13 marca 2020 r.</p>
-        <p><a href="/static/download/Nowe_zarzadzenie_13_marca_2020.pdf">Zarządzenie - plik do pobrania</a></p>
+        <p><a href="/static/download/Nowe_zarzadzenie_13_marca_2020.pdf">
+        Zarządzenie - plik do pobrania</a></p>
         <p>
             <span style="float: left;">Sandomierz, 15.&nbsp;marca 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
--- last_post.php
+++ last_post.php
@@ -1,80 +1,5 @@
 <div class="card">
     <div class="card-header">
-        <h3>24.&nbsp;maja 2020&nbsp;r.</h3>
-        <h3>VII NIEDZIELA WIELKANOCNA - Rok A</h3>
-        <h3>Uroczystość Wniebowstąpienia Pańskiego</h3>
-    </div>
-    <div class="col">
-        <p>1. <strong>Jak wiemy, od&nbsp;17&nbsp;maja -
-        w&nbsp;kościele może być na&nbsp;Mszy Świętej więcej osób.</strong>
-        1&nbsp;osoba na&nbsp;10 m<sup>2</sup>. U&nbsp;nas w&nbsp;kościele
-        może uczestniczyć <strong>72&nbsp;osoby</strong> – oczywiście
-        w&nbsp;rozproszeniu po&nbsp;całej świątyni. <strong>Zapraszamy
-        zatem korzystajcie z&nbsp;tej możliwości.</strong> Gdyby było już
-        więcej osób, pozostańmy na&nbsp;zewnątrz kościoła w&nbsp;odstępach
-        od&nbsp;siebie – ok.&nbsp;2&nbsp;m. Będzie włączone nagłośnienie,
-        aby uczestnicy wszystko słyszeli; z&nbsp;komunią Świętą kapłan
-        będzie stał na&nbsp;podeście przy drzwiach głównych.</p>
-        <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
-        i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia w&nbsp;tym
-        okresie niech sprawdza w&nbsp;gablocie na&nbsp;wywieszce.</p>
-        <p>3. Przypominamy, że&nbsp;Ks.&nbsp;bp&nbsp;udzielił dyspensy
-        od&nbsp;udziału we&nbsp;Mszy Świętej w&nbsp;niedziele
-        do&nbsp;odwołania. Zachęca nas jednak do&nbsp;udziału we&nbsp;Mszy
-        Świętej korzystając z&nbsp;mediów zarówno ogólnopolskich, jak
-        i&nbsp;lokalnych.</p>
-        <p>4. Kościół jest otwarty przez cały dzień, zachęcamy
-        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
-        epidemii.</p>
-        <p>5. Nabożeństwo majowe w&nbsp;dni powszednie odprawiamy
-        o&nbsp;godzinie 17.30.</p>
-        <p>6. Katechizacja przygotowująca do&nbsp;Sakramentu małżeństwa dla
-        tych, którzy planują brać ślub do&nbsp;września jest prowadzona
-        przez Internet. Katechezy są&nbsp;we&nbsp;wtorek i&nbsp;piątek
-        o&nbsp;godzinie 20.00. Informacja szczegółowa dla zainteresowanych
-        jest w&nbsp;gablocie.</p>
-        <p>7. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
-        wspólnoty do&nbsp;wieczności Teresę Świątańską. Polećmy
-        ją&nbsp;Bożemu miłosierdziu odmawiając: <em>Wieczny odpoczynek...</em></p>
-        <p>8. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;złożoną
-        ofiarę na&nbsp;sprzątanie oraz na&nbsp;kwiaty, za&nbsp;pracę dla
-        dobra parafii Panom: Tadeuszowi i&nbsp;Ryszardowi, za&nbsp;podlewanie
-        kwiatów na&nbsp;rabatkach Rodzinie Daniela i&nbsp;Agaty wraz
-        z&nbsp;dziećmi: Gabrysiem i&nbsp;Antosiem. <em>Bóg zapłać</em>
-        za&nbsp;ofiary dla Parafii, które przesyłacie na&nbsp;konto bankowe
-        lub składacie w&nbsp;zakrystii.</p>
-        <p>9. Informujemy, że&nbsp;Jadłodajnia Romana już pracuje, jak
-        dawniej, tzn. z&nbsp;możliwością konsumpcji posiłków na&nbsp;miejscu.
-        Jeżeli będzie taka potrzeba, będzie możliwość spożywania posiłków
-        na&nbsp;dużej sali, aby zachować odpowiednie odległości między
-        stolikami. Zapraszamy.</p>
-        <p>10. Pielgrzymka do&nbsp;Medjugorie została odwołana z&nbsp;racji
-        koronawirusa. Ks.&nbsp;Krzysztof będzie się kontaktował telefonicznie
-        z&nbsp;zapisanymi osobami.</p>
-        <p>11. Bardzo prosimy poruszających się po&nbsp;placu kościelnym
-        samochodami, aby nie wjeżdżać na&nbsp;rabatki kwiatowe i&nbsp;nie
-        niszczyć kwiatów. Pomyśl sobie, że&nbsp;Ciebie ktoś przejeżdża
-        samochodem, jak byś się czuł - podobnie cierpią kwiaty przejechane
-        przez kierowcę.</p>
-        <p>12. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 5
-        od&nbsp;1 do&nbsp;10 włącznie.</p>
-        <p>13. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>14. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
-        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
-        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
-        w&nbsp;waszych intencjach.</p>
-        <p>
-            <span style="float: left;">Sandomierz, 24.&nbsp;maja 2020&nbsp;r.</span>
-            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
-        </p>
-    </div>
-</div>
-<br />
-
-<div class="card">
-    <div class="card-header">
         <h3>
             Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy Świętej
         </h3>
@@ -122,15 +47,16 @@
         obowiązujących przepisów sanitarnych – adoracje
         oraz&nbsp;inne formy kultu Jezusa Eucharystycznego.</p>
         <p>&nbsp;</p>
-        <p style="text-align: right;"><em>Contrariis quibuslibet minime obstantibus.</em></p>
+        <p style="text-align: right;"><em>Contrariis quibuslibet
+        minime obstantibus.</em></p>
         <p style="text-align: right;">Sandomierz, 29 maja 2020 r.</p>
         <p>&nbsp;</p>
         <p style="text-align: right;">Biskup Sandomierski<br>
         Krzysztof Nitkiewicz</p>
         <p style="text-align: right;">Kanclerz<br>
         ks. Roman Janiec</p>
-        <p><a href="https://diecezjasandomierska.pl/wp-content/uploads/2020/05/Dekret-o-obowiązku-uczestnictwa-we-Mszy-św..pdf">Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy św.</a></p>
-
+        <p><a href="https://diecezjasandomierska.pl/wp-content/uploads/2020/05/Dekret-o-obowiązku-uczestnictwa-we-Mszy-św..pdf">
+        Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy św.</a></p>
         <p>
             <a href="https://diecezjasandomierska.pl/dekret-o-obowiazku-uczestnictwa-we-mszy-sw/">
             Link do dekretu na stronie Diecezji Sandomierskiej</a><br />
@@ -143,52 +69,81 @@
 
 <div class="card">
     <div class="card-header">
-        <h3>
-            Zwiększono dopuszczalną liczbę osób
-        </h3>
+        <h3>31.&nbsp;maja 2020&nbsp;r.</h3>
+        <h3>NIEDZIELA Zesłania Ducha Świętego - Rok A</h3>
     </div>
     <div class="col">
-        <h4>
-            Informujemy, że&nbsp;zgodnie z&nbsp;nowymi przepisami
-            na&nbsp;Mszach św. w&nbsp;naszym kościele może uczestniczyć
-            maksymalnie <strong>72&nbsp;osoby.</strong>
-        </h4>
+        <p>1. <strong>Jak wiemy, od&nbsp;dzisiejszego dnia nie ma&nbsp;ograniczeń
+        co&nbsp;do&nbsp;liczby uczestników we&nbsp;Mszy Świętej
+        w&nbsp;kościele. Pamiętajmy zatem, że&nbsp;Pan Bóg
+        w&nbsp;3&nbsp;przykazaniu mówi nam: "Pamiętaj abyś dzień święty
+        święcił". Zapraszamy do&nbsp;udziału we&nbsp;Mszy Świętej
+        w&nbsp;każdą niedzielę i&nbsp;święto nakazane wszystkich parafian:
+        dzieci, młodzież i&nbsp;dorosłych. Ks.&nbsp;biskup Ordynariusz
+        w&nbsp;wydanym ostatnio dekrecie informuje...</strong></p>
+        <p>2. <strong>Jutro Święto Matki Kościoła. U&nbsp;nas Msza Święta
+        o&nbsp;godzinie 6.30, 9.00 i&nbsp;18.00. Czcicieli Matki Bożej
+        zapraszamy do&nbsp;udziału we&nbsp;Mszy Świętej.</strong></p>
+        <p>3. W&nbsp;najbliższą niedzielę – Niedzielę Trójcy Świętej – kończy
+        się okres spowiedzi wielkanocnej. Przypominamy, że&nbsp;przykazanie
+        kościelne uczy nas: <strong>przynajmniej raz w&nbsp;roku spowiadać
+        się i&nbsp;w&nbsp;czasie wielkanocnym komunię świętą
+        przyjmować</strong>. Kto nie był jeszcze w&nbsp;tym roku
+        u&nbsp;spowiedzi Świętej wielkanocnej, niech to&nbsp;uczyni
+        w&nbsp;tym tygodniu. Spowiadamy codziennie przed Mszą świętą rano
+        i&nbsp;wieczorem.</p>
+        <p>4. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
+        i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia, niech
+        sprawdza w&nbsp;gablocie na&nbsp;wywieszce.</p>
+        <p>5. W&nbsp;tym tygodniu wypada pierwszy czwartek, piątek
+        i&nbsp;sobota miesiąca. Spowiedź św.&nbsp;w&nbsp;pierwszy piątek
+        od&nbsp;godz.&nbsp;17.00, nabożeństwo I&nbsp;sobotnie o&nbsp;godzinie
+        17.00. Z&nbsp;posługą do&nbsp;chorych pójdziemy w&nbsp;tym
+        tygodniu.</p>
+        <p>6. Nabożeństwo do&nbsp;Serca Pana Jezusa przez cały czerwiec
+        będziemy odprawiać o&nbsp;17.40.</p>
+        <p>7. W&nbsp;Boże Ciało procesje będą przy kościołach. Prosimy Koło
+        Gospodyń <strong>Chwałowianki o&nbsp;przygotowanie I-go ołtarza,
+        Kobierzanki – II-go, Koło Gospodyń Wiejskich Milczany – III,
+        a&nbsp;Zespół, który co&nbsp;roku przygotowywał ołtarz IV –
+        ołtarza na&nbsp;podeście przy wejściu do&nbsp;kościoła</strong>
+        i&nbsp;tutaj będzie zakończenie procesji. Zainteresowani ołtarzami
+        kontaktują się z&nbsp;Księdzem Krzysztofem.</p>
+        <p>8. Kościół jest otwarty przez cały dzień, zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii.</p>
+        <p>9. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej
+        wspólnoty do&nbsp;wieczności Pawła Grzesiaka. Polećmy go&nbsp;Bożemu
+        miłosierdziu odmawiając: <em>wieczny odpoczynek...</em></p>
+        <p>10. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła i&nbsp;świeże
+        kwiaty, za&nbsp;pracę dla dobra parafii Panom: Tadeuszowi
+        i&nbsp;Ryszardowi, za&nbsp;podlewanie kwiatów na&nbsp;rabatkach
+        Rodzinie Daniela i&nbsp;Agaty wraz z&nbsp;dziećmi: Gabrysiem
+        i&nbsp;Antosiem. <em>Bóg zapłać</em> za&nbsp;ofiary dla Parafii,
+        które przesyłacie na&nbsp;konto bankowe lub składacie
+        w&nbsp;zakrystii.</p>
+        <p>11. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> rodziny z&nbsp;bloku Maciejowskiego 5
+        od&nbsp;11 do&nbsp;20.</p>
+        <p>12. Informujemy, że&nbsp;Jadłodajnia Romana już pracuje, jak
+        dawniej, tzn. z&nbsp;możliwością konsumpcji posiłków
+        na&nbsp;miejscu.</p>
+        <p>13. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>14. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>15. Pierwsza Komunia Święta - zgodnie z&nbsp;wolą rodziców
+        przewidywana jest 23&nbsp;sierpnia. W&nbsp;związku z&nbsp;tym
+        zapraszamy dzieci wraz z&nbsp;rodzicem (rodzicami) na&nbsp;spotkanie
+        przygotowujące do&nbsp;przyjęcia Jezusa Chrystusa w&nbsp;Eucharystii.
+        Spotkanie będzie w&nbsp;piątek 5&nbsp;czerwca,
+        o&nbsp;godz.&nbsp;16.00.</p>
         <p>
-            <span style="float: left;">Sandomierz, 17.&nbsp;maja 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 31.&nbsp;maja 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
         </p>
     </div>
 </div>
 <br />
-
-<div class="card">
-    <div class="card-header">
-        <h3>15.&nbsp;marca 2020&nbsp;r.</h3>
-        <h3>Nowe zarządzenie</h3>
-    </div>
-    <div class="col">
-        <p>Nowe zarządzenie z dnia 13 marca 2020 r.</p>
-        <p><a href="/static/download/Nowe_zarzadzenie_13_marca_2020.pdf">Zarządzenie - plik do pobrania</a></p>
-        <p>
-            <span style="float: left;">Sandomierz, 15.&nbsp;marca 2020&nbsp;r.</span>
-            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
-        </p>
-    </div>
-</div>
-<br />
-
-<div class="card">
-    <div class="card-header">
-        <h3>12.&nbsp;marca 2020&nbsp;r.</h3>
-        <h3>Komunikat dla Diecezji Sandomierskiej</h3>
-    </div>
-    <div class="col">
-        <p>Dekret Biskupa Sandomierskiego z dnia 12 marca 2020 r.</p>
-        <p><img src="/static/download/FB_IMG_1584126347055.jpg"/></p>
-        <p><a href="/static/download/Dekret_003_12_marca_2020.pdf">Dekret - plik do pobrania</a></p>
-        <p>
-            <span style="float: left;">Sandomierz, 12.&nbsp;marca 2020&nbsp;r.</span>
-            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
-        </p>
-    </div>
-</div>
