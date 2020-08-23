27c1b903152856dcad3c754e5882669adccb2a3a
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-08-23 19:38:27 +0100

    2020-08-23

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,69 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 9.&nbsp;sierpnia 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 16.&nbsp;sierpnia 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>16.&nbsp;sierpnia 2020&nbsp;r.</h3>
+        <h3>XX NIEDZIELA ZWYKŁA - Rok A</h3>
+    </div>
+    <div class="col">
+        <h3>W&nbsp;lipcu i&nbsp;sierpniu nie będzie Mszy Świętej
+        o&nbsp;godzinie 12:30.</h3>
+
+        <p>1. <strong>Jak wiemy, od&nbsp;30&nbsp;maja nie ma&nbsp;ograniczeń
+        co&nbsp;do&nbsp;liczby uczestników we&nbsp;Mszy Świętej
+        w&nbsp;kościele. Pamiętajmy zatem, że&nbsp;Pan Bóg
+        w&nbsp;3&nbsp;przykazaniu mówi nam: "Pamiętaj, abyś dzień święty
+        święcił". Zapraszamy do&nbsp;udziału we&nbsp;Mszy Świętej
+        w&nbsp;każdą niedzielę i&nbsp;święto nakazane wszystkich parafian:
+        dzieci, młodzież i&nbsp;dorosłych.</strong></p>
+        <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
+        i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia, niech
+        sprawdza w&nbsp;gablocie na&nbsp;wywieszce. Sposobność
+        do&nbsp;spowiedzi przed Mszą&nbsp;Św. zarówno rano, jak
+        i&nbsp;wieczorem.</p>
+        <p>3. Kościół jest otwarty przez cały dzień, zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii.</p>
+        <p>4. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej Parafii
+        do&nbsp;wieczności Eugeniusza Łukaszewskiego. <em>Wieczny odpoczynek
+        racz mu&nbsp;dać Panie...</em></p>
+        <p>5. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, ofiarę, dary
+        stołu i&nbsp;wszelkie dobro. Rodzinie Daniela i&nbsp;Agaty wraz
+        z&nbsp;dziećmi: Gabrysiem i&nbsp;Antosiem za&nbsp;pielęgnację
+        kwiatów, <strong><em>Bóg zapłać</em> Panu Leszkowi, Rafałowi,
+        Ryszardowi i&nbsp;Tadeuszowi za&nbsp;pracę dla dobra
+        Parafii.</strong> Wam <strong>Kochani</strong> bardzo dziękuję
+        za&nbsp;ofiary, które składacie na&nbsp;tacę, przesyłacie
+        na&nbsp;konto bankowe lub przekazujecie w&nbsp;zakrystii.</p>
+        <p>6. <strong>Do&nbsp;sprzątania prosimy w&nbsp;tym tygodniu
+        Rodziców dzieci, które przystępują w&nbsp;niedzielę do&nbsp;I–szej
+        Komunii Świętej.</strong></p>
+        <p>7. Dzieci z&nbsp;Rodzicami i&nbsp;członkami Rodzin przychodzą
+        do&nbsp;spowiedzi w&nbsp;piątek od&nbsp;godziny 16 do&nbsp;18.
+        Zachęcamy do&nbsp;korzystania ze&nbsp;spowiedzi codziennie
+        od&nbsp;17.30 do&nbsp;18.</p>
+        <p>8. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>9. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej
+        modlitwie w&nbsp;waszych intencjach.</p>
+
+        <h3>Pierwsza Komunia Święta w&nbsp;naszej Parafii, zgodnie
+        z&nbsp;wolą Rodziców będzie 23&nbsp;sierpnia o&nbsp;godzinie
+        11.15. Prośba do&nbsp;tych wiernych, którzy lubią chodzić
+        na&nbsp;Mszę Świętą o&nbsp;tej godzinie, aby tego dnia
+        przyszli na&nbsp;inną godzinę.</h3>
+        <p>
+            <span style="float: left;">Sandomierz, 16.&nbsp;sierpnia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>9.&nbsp;sierpnia 2020&nbsp;r.</h3>
@@ -697,6 +759,75 @@ include 'head.php';
         <p>
             <span style="float: left;">Sandomierz, 31.&nbsp;maja 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
+<div class="card">
+    <div class="card-header">
+        <h3>
+            Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy Świętej
+        </h3>
+    </div>
+    <div class="col">
+        <p>
+            29&nbsp;maja 2020&nbsp;r. Biskup Sandomierski Krzysztof
+            Nitkiewicz wydał dekret o&nbsp;tym, że&nbsp;dyspensa
+            od&nbsp;obowiązku uczestnictwa we&nbsp;Mszy&nbsp;św.
+            w&nbsp;niedziele i&nbsp;dni nakazane, udzielona 12&nbsp;marca
+            br. i&nbsp;przedłużona 21&nbsp;marca, obowiązuje jedynie
+            <strong>do&nbsp;dnia 31&nbsp;maja br. włącznie</strong>.
+        </p>
+        <p style="text-align: center;"><strong>DEKRET</strong><br>
+            <strong>o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy
+            św.</strong>
+        </p>
+        <p>W&nbsp;Najświętszej Eucharystii, która&nbsp;urzeczywistnia
+        Komunię z&nbsp;Bogiem i&nbsp;jedność Ludu Bożego jest obecny
+        sam Chrystus, nasza Pascha i&nbsp;Chleb Żywy (por. PO&nbsp;5,
+        KKK 1325).</p>
+        <p>Dlatego przyjmując z&nbsp;radością zniesienie ograniczeń
+        sanitarnych dotyczących liczby uczestników liturgii
+        odprawianej wewnątrz świątyni, postanawiam, że&nbsp;dyspensa
+        od&nbsp;obowiązku uczestnictwa we&nbsp;Mszy
+        św.&nbsp;w&nbsp;niedziele i&nbsp;dni nakazane, udzielona
+        12&nbsp;marca br.&nbsp;i&nbsp;przedłużona 21 marca,
+        obowiązuje jedynie do&nbsp;dnia 31 maja br.&nbsp;włącznie.</p>
+        <p>Jednocześnie udzielam dyspensy od&nbsp;obowiązku
+        uczestnictwa we&nbsp;Mszy św.&nbsp;w&nbsp;niedziele
+        i&nbsp;dni nakazane osobom: w&nbsp;podeszłym wieku, mającym
+        objawy infekcji (np.&nbsp;podwyższona temperatura, kaszel,
+        katar itd.), przebywającym na&nbsp;kwarantannie i&nbsp;tym,
+        które z&nbsp;innych powodów są szczególnie podatne
+        na&nbsp;zakażenie bądź obawiając się jego, unikają
+        przebywania w&nbsp;miejscach publicznych. Dyspensa
+        ta&nbsp;będzie obowiązywała do&nbsp;końca czerwca br.</p>
+        <p>Wierni uczestniczący w&nbsp;liturgii mają obowiązek
+        przestrzegania wszystkich przepisów sanitarnych wydanych
+        przez&nbsp;władze państwowe.</p>
+        <p>Księża Proboszczowie i&nbsp;Rektorzy kościołów dołożą
+        wszelkich starań, aby zasady te zostały zachowane. Powinni
+        jednocześnie zachęcić wiernych do&nbsp;pełnego uczestnictwa
+        we&nbsp;Mszy św., organizując – z&nbsp;zachowaniem
+        obowiązujących przepisów sanitarnych – adoracje
+        oraz&nbsp;inne formy kultu Jezusa Eucharystycznego.</p>
+        <p>&nbsp;</p>
+        <p style="text-align: right;"><em>Contrariis quibuslibet
+        minime obstantibus.</em></p>
+        <p style="text-align: right;">Sandomierz, 29 maja 2020 r.</p>
+        <p>&nbsp;</p>
+        <p style="text-align: right;">Biskup Sandomierski<br>
+        Krzysztof Nitkiewicz</p>
+        <p style="text-align: right;">Kanclerz<br>
+        ks. Roman Janiec</p>
+        <p><a href="https://diecezjasandomierska.pl/wp-content/uploads/2020/05/Dekret-o-obowiązku-uczestnictwa-we-Mszy-św..pdf">
+        Dekret o&nbsp;obowiązku uczestnictwa we&nbsp;Mszy św.</a></p>
+        <p>
+            <a href="https://diecezjasandomierska.pl/dekret-o-obowiazku-uczestnictwa-we-mszy-sw/">
+            Link do dekretu na stronie Diecezji Sandomierskiej</a><br />
+            <a href="static/download/Dekret-o-obowiazku-uczestnictwa-we-Mszy-sw_2020-05-29.pdf">
+            Plik do pobrania z naszego serwera</a>
         </p>
     </div>
 </div>
