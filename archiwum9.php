8b77c10c053886e955cd2ffc2c3b28da5e922119
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-09-06 11:50:39 +0100

    2020-09-06

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,68 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 23.&nbsp;sierpnia 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 30.&nbsp;sierpnia 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>30.&nbsp;sierpnia 2020&nbsp;r.</h3>
+        <h3>XXII NIEDZIELA ZWYKŁA - Rok A</h3>
+    </div>
+    <div class="col">
+        <h3>Wakacje minęły. Msza Święta na&nbsp;rozpoczęcie roku
+        szkolnego 2020/21 będzie we&nbsp;wtorek o&nbsp;godzinie 8.00.
+        Zapraszamy do&nbsp;udziału dzieci, młodzież, Rodziców,
+        Nauczycieli.</h3>
+
+        <p>1. <strong>Jak wiemy, od&nbsp;30&nbsp;maja nie ma&nbsp;ograniczeń
+        co&nbsp;do&nbsp;liczby uczestników we&nbsp;Mszy Świętej
+        w&nbsp;kościele. Pamiętajmy zatem, że&nbsp;Pan Bóg
+        w&nbsp;3&nbsp;przykazaniu mówi nam: "Pamiętaj, abyś dzień święty
+        święcił". Zapraszamy do&nbsp;udziału we&nbsp;Mszy Świętej
+        w&nbsp;każdą niedzielę i&nbsp;święto nakazane wszystkich parafian:
+        dzieci, młodzież i&nbsp;dorosłych.</strong></p>
+        <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
+        i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia, niech
+        sprawdza w&nbsp;gablocie na&nbsp;wywieszce. Sposobność
+        do&nbsp;spowiedzi przed Mszą&nbsp;Św. zarówno rano, jak
+        i&nbsp;wieczorem.</p>
+        <p>3. Kościół jest otwarty przez cały dzień, zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii.</p>
+        <p>4. W&nbsp;tym tygodniu jest I&nbsp;czwartek, piątek i&nbsp;sobota
+        miesiąca września. W&nbsp;I&nbsp;piątek okazja do&nbsp;spowiedzi
+        od&nbsp;godziny 17.00.</p>
+        <p>5. 7&nbsp;września jest piesza pielgrzymka do&nbsp;Sulisławic.</p>
+        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
+        z&nbsp;naszej wspólnoty Mieczysława Kunkiewicza, którego pogrzeb
+        będzie we&nbsp;wtorek o&nbsp;godzinie 14.00. <em>Wieczny odpoczynek
+        racz mu&nbsp;dać Panie...</em></p>
+        <p>7. <em>Bóg zapłać</em> za&nbsp;rodzinne sprzątanie kościoła oraz
+        zamówioną Mszę Świętą w&nbsp;intencjach wczoraj sprzątających rodzin,
+        która będzie odprawiona 7&nbsp;września o&nbsp;godzinie 18.00.
+        <em>Bóg zapłać</em> Panu Piotrowi za&nbsp;zakup 2&nbsp;drzew
+        i&nbsp;krzewów i&nbsp;posadzenie na&nbsp;placu wraz
+        z&nbsp;P.&nbsp;Halinką, jako rekompensata za&nbsp;wycięcie drzewa,
+        które wrastało i&nbsp;zagrażało rozwaleniem WC&nbsp;wybudowanemu
+        na&nbsp;placu kościelnym. Wam <strong>Kochani</strong> bardzo
+        dziękuję za&nbsp;ofiary, które składacie na&nbsp;tacę, przesyłacie
+        na&nbsp;konto bankowe lub przekazujecie w&nbsp;zakrystii.</p>
+        <p>8. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 2
+        od&nbsp;31 do&nbsp;40 włącznie.</p>
+        <p>9. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>
+            <span style="float: left;">Sandomierz, 30.&nbsp;sierpnia 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. proboszcz Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>23.&nbsp;sierpnia 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,16 +1,24 @@
-
-
-
 <div class="card">
     <div class="card-header">
-        <h3>30.&nbsp;sierpnia 2020&nbsp;r.</h3>
-        <h3>XXII NIEDZIELA ZWYKŁA - Rok A</h3>
+        <h3>6.&nbsp;września 2020&nbsp;r.</h3>
+        <h3>XXIII NIEDZIELA ZWYKŁA - Rok A</h3>
     </div>
     <div class="col">
-        <h3>Wakacje minęły. Msza Święta na&nbsp;rozpoczęcie roku
-        szkolnego 2020/21 będzie we&nbsp;wtorek o&nbsp;godzinie 8.00.
-        Zapraszamy do&nbsp;udziału dzieci, młodzież, Rodziców,
-        Nauczycieli.</h3>
+        <h3>W&nbsp;związku z&nbsp;obowiązującym w&nbsp;naszej
+        diecezji prawem partykularnym stanowiącym,
+        że&nbsp;po&nbsp;osiągnięciu 70&nbsp;lat życia (w&nbsp;moim
+        przypadku 31.08.2020&nbsp;r.) przechodzimy w&nbsp;stan
+        emerytalny, złożyłem pismo do&nbsp;Kurii Diecezjalnej
+        o&nbsp;zrzeczeniu z&nbsp;urzędu proboszcza parafii
+        Podwyższenia Krzyża Świętego w&nbsp;Sandomierzu.
+        2&nbsp;września otrzymałem pismo następującej treści …<br />
+        <img src="static/download/20200906_072048.webp"
+             alt="Pismo z Kurii Diecezjalnej"
+             style="max-width: 100%;"/>
+        w&nbsp;związku z&nbsp;powyższym oczekujemy
+        na&nbsp;wyznaczenie przez księdza biskupa proboszcza dla
+        naszej parafii, ja&nbsp;do&nbsp;jego przyjścia pełnię
+        obowiązki administratora.</h3>
 
         <p>1. <strong>Jak wiemy, od&nbsp;30&nbsp;maja nie ma&nbsp;ograniczeń
         co&nbsp;do&nbsp;liczby uczestników we&nbsp;Mszy Świętej
@@ -27,30 +35,36 @@
         <p>3. Kościół jest otwarty przez cały dzień, zachęcamy
         do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
         epidemii.</p>
-        <p>4. W&nbsp;tym tygodniu jest I&nbsp;czwartek, piątek i&nbsp;sobota
-        miesiąca września. W&nbsp;I&nbsp;piątek okazja do&nbsp;spowiedzi
-        od&nbsp;godziny 17.00.</p>
-        <p>5. 7&nbsp;września jest piesza pielgrzymka do&nbsp;Sulisławic.</p>
-        <p>6. W&nbsp;minionym tygodniu odwołał Pan Bóg do&nbsp;wieczności
-        z&nbsp;naszej wspólnoty Mieczysława Kunkiewicza, którego pogrzeb
-        będzie we&nbsp;wtorek o&nbsp;godzinie 14.00. <em>Wieczny odpoczynek
-        racz mu&nbsp;dać Panie...</em></p>
-        <p>7. <em>Bóg zapłać</em> za&nbsp;rodzinne sprzątanie kościoła oraz
-        zamówioną Mszę Świętą w&nbsp;intencjach wczoraj sprzątających rodzin,
-        która będzie odprawiona 7&nbsp;września o&nbsp;godzinie 18.00.
-        <em>Bóg zapłać</em> Panu Piotrowi za&nbsp;zakup 2&nbsp;drzew
-        i&nbsp;krzewów i&nbsp;posadzenie na&nbsp;placu wraz
-        z&nbsp;P.&nbsp;Halinką, jako rekompensata za&nbsp;wycięcie drzewa,
-        które wrastało i&nbsp;zagrażało rozwaleniem WC&nbsp;wybudowanemu
-        na&nbsp;placu kościelnym. Wam <strong>Kochani</strong> bardzo
-        dziękuję za&nbsp;ofiary, które składacie na&nbsp;tacę, przesyłacie
+        <p>4. Wakacje minęły – wracamy już od&nbsp;dzisiaj
+        do&nbsp;odprawiania o&nbsp;12.30 Mszy Świętej dla dzieci
+        z&nbsp;rodzicami.</p>
+        <p>5. O&nbsp;15.30 nabożeństwo różańcowe i&nbsp;zmiana tajemnic
+        różańcowych dla członków Kół Żywego Różańca.</p>
+        <p>6. Jutro o&nbsp;godz.&nbsp;7.30 w&nbsp;Katedrze Msza Święta
+        i&nbsp;piesza pielgrzymka do&nbsp;Sulisławic.</p>
+        <p>7. We&nbsp;wtorek 8&nbsp;września obchodzimy liturgiczne święto
+        Narodzenia NMP. W&nbsp;Katedrze odpust parafialny. U&nbsp;nas
+        porządek Mszy Świętych, jak w&nbsp;dni powszednie: 6.30 i&nbsp;18.00.
+        Czcicieli Matki Bożej zapraszamy do&nbsp;udziału
+        w&nbsp;Eucharystii.</p>
+        <p>8. Za&nbsp;tydzień w&nbsp;poniedziałek 14&nbsp;września
+        w&nbsp;naszej Parafii odpust tytułu kościoła Podwyższenia Krzyża
+        Świętego. Msza Święta z&nbsp;kazaniem odpustowym o&nbsp;godzinie 9.00
+        i&nbsp;18.00. Zaplanujcie sobie tak dzień, aby jak w&nbsp;dawnych
+        latach uczestniczyć w&nbsp;Eucharystii i&nbsp;zyskać odpust
+        zupełny.</p>
+        <p>9. <em>Bóg zapłać</em> za&nbsp;złożoną przez dwie Panie ofiarę
+        na&nbsp;sprzątanie kościoła. <em>Bóg zapłać</em> Panom Wacławowi,
+        Ryszardowi i&nbsp;Tadeuszowi za&nbsp;prace dla dobra Parafii
+        i&nbsp;dla mnie. Wam <strong>Kochani</strong> bardzo dziękuję
+        za&nbsp;ofiary, które składacie na&nbsp;tacę, przesyłacie
         na&nbsp;konto bankowe lub przekazujecie w&nbsp;zakrystii.</p>
-        <p>8. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        <p>10. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
         na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Maciejowskiego 2
-        od&nbsp;31 do&nbsp;40 włącznie.</p>
-        <p>9. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        od&nbsp;41 do&nbsp;50 włącznie.</p>
+        <p>11. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
         Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>10. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>12. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
         urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
         składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
         w&nbsp;waszych intencjach.</p>
