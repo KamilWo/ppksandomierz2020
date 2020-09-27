114c70071cf3d585b651bd7b8e0d227bf73bdf35
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-09-27 14:20:53 +0100

    2020-09-27

--- archiwum9.php
+++ archiwum9.php
@@ -13,7 +13,87 @@ include 'head.php';
             <!-- treść główna -->
             <div class="col">
                 <h2 class="first-content-header">Archiwum wiadomości 2020 rok</h2>
-                <h2>Okres 1.&nbsp;stycznia 2020 - 13.&nbsp;września 2020 r.</h2>
+                <h2>Okres 1.&nbsp;stycznia 2020 - 20.&nbsp;września 2020 r.</h2>
+<div class="card">
+    <div class="card-header">
+        <h3>20.&nbsp;września 2020&nbsp;r.</h3>
+        <h3>XXV NIEDZIELA ZWYKŁA - Rok A</h3>
+    </div>
+    <div class="col">
+        <h3>W&nbsp;związku z&nbsp;obowiązującym w&nbsp;naszej diecezji prawem
+        partykularnym stanowiącym, że&nbsp;po&nbsp;osiągnięciu 70&nbsp;lat
+        życia (w&nbsp;moim przypadku 31.08.2020&nbsp;r.) przechodzimy
+        w&nbsp;stan emerytalny, złożyłem pismo do&nbsp;Kurii Diecezjalnej
+        o&nbsp;zrzeczeniu z&nbsp;urzędu proboszcza parafii Podwyższenia
+        Krzyża Świętego w&nbsp;Sandomierzu. 2&nbsp;września otrzymałem pismo
+        następującej treści
+        <img src="static/download/20200906_072048.webp"
+             alt="Pismo z Kurii Diecezjalnej"
+             style="max-width: 100%;"/>
+        (pismo w&nbsp;załączeniu w&nbsp;ogłoszeniach z&nbsp;niedzieli XXIII).
+        W&nbsp;związku z&nbsp;powyższym oczekujemy na&nbsp;wyznaczenie przez
+        księdza biskupa proboszcza dla naszej parafii, ja&nbsp;do&nbsp;jego
+        przyjścia pełnię obowiązki administratora.<br /><br />
+
+        Zachęcam do&nbsp;modlitwy w&nbsp;intencjach kapłana, który będzie
+        kolejnym proboszczem tej parafii.</h3>
+
+
+        <p>1. <strong>Jak wiemy, od&nbsp;30&nbsp;maja nie ma&nbsp;ograniczeń
+        co&nbsp;do&nbsp;liczby uczestników we&nbsp;Mszy Świętej
+        w&nbsp;kościele. Pamiętajmy zatem, że&nbsp;Pan Bóg
+        w&nbsp;3&nbsp;przykazaniu mówi nam: "Pamiętaj, abyś dzień święty
+        święcił". Zapraszamy do&nbsp;udziału we&nbsp;Mszy Świętej
+        w&nbsp;każdą niedzielę i&nbsp;święto nakazane wszystkich parafian:
+        dzieci, młodzież i&nbsp;dorosłych.</strong></p>
+        <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
+        i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia, niech
+        sprawdza w&nbsp;gablocie na&nbsp;wywieszce. Sposobność
+        do&nbsp;spowiedzi przed Mszą Św. zarówno rano, jak
+        i&nbsp;wieczorem.</p>
+        <p>3. Kościół jest otwarty codziennie przez cały dzień. Zachęcamy
+        do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
+        epidemii i&nbsp;potrzebne łaski dla ducha i&nbsp;dla ciała.</p>
+        <p>4. Po&nbsp;wakacyjnej przerwie odprawiamy o&nbsp;12.30 Mszę
+        Świętą dla dzieci z&nbsp;rodzicami. Zapraszamy do&nbsp;licznego
+        udziału.</p>
+        <p>5. Jeśli są&nbsp;tacy Rodzice, którzy nie zapisali jeszcze swoich
+        dzieci do&nbsp;I–Komunii Świętej, mogą to&nbsp;uczynić dzisiaj lub
+        w&nbsp;ciągu tygodnia w&nbsp;zakrystii czy kancelarii parafialnej.</p>
+        <p>6. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, kwiaty, ofiarę
+        na&nbsp;Mszę Świętą za&nbsp;sprzątających, która będzie odprawiona
+        dzisiaj o&nbsp;godzinie 16.00, <em>Bóg zapłać</em>
+        za&nbsp;wyciągnięcie nieczystości z&nbsp;parafialnego szaletu,
+        za&nbsp;zakup krzewów, które po&nbsp;kontroli Urzędu Miejskiego mamy
+        jeszcze dosadzić w&nbsp;związku z&nbsp;wyciętym drzewem, które
+        rozwalało nam szalet. Wam <strong>Kochani</strong> bardzo dziękuję
+        za&nbsp;ofiary, które składacie na&nbsp;tacę, przesyłacie
+        na&nbsp;konto bankowe lub przekazujecie w&nbsp;zakrystii.</p>
+        <p>7. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Mickiewicza 43
+        od&nbsp;1 do&nbsp;27 – mieszkańców z&nbsp;klatki 1, 2 i&nbsp;3.</p>
+        <p>8. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej Parafii
+        Andrzeja Łukaszka. <em>Wieczny odpoczynek racz mu&nbsp;dać
+        Panie...</em></p>
+        <p>9. Taca w&nbsp;przyszłą niedzielę zgodnie z&nbsp;dekretem
+        o&nbsp;zobowiązaniach finansowych Parafii jest przeznaczona
+        na&nbsp;remont Katedry w&nbsp;Sandomierzu.</p>
+        <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>11. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
+        składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
+        w&nbsp;waszych intencjach.</p>
+        <p>12. Informacja Pana Burmistrza Miasta Sandomierza i&nbsp;Pana
+        Wójta Gminy Obrazów o&nbsp;spisie rolnym...</p>
+        <p>
+            <span style="float: left;">Sandomierz, 20.&nbsp;września 2020&nbsp;r.</span>
+            <span style="float: right; text-align: right;">ks. administrator Józef Śmigiel</span>
+        </p>
+    </div>
+</div>
+<br />
+
 <div class="card">
     <div class="card-header">
         <h3>13.&nbsp;września 2020&nbsp;r.</h3>
--- last_post.php
+++ last_post.php
@@ -1,7 +1,7 @@
 <div class="card">
     <div class="card-header">
-        <h3>20.&nbsp;września 2020&nbsp;r.</h3>
-        <h3>XXV NIEDZIELA ZWYKŁA - Rok A</h3>
+        <h3>27.&nbsp;września 2020&nbsp;r.</h3>
+        <h3>XXVI NIEDZIELA ZWYKŁA - Rok A</h3>
     </div>
     <div class="col">
         <h3>W&nbsp;związku z&nbsp;obowiązującym w&nbsp;naszej diecezji prawem
@@ -22,7 +22,6 @@
         Zachęcam do&nbsp;modlitwy w&nbsp;intencjach kapłana, który będzie
         kolejnym proboszczem tej parafii.</h3>
 
-
         <p>1. <strong>Jak wiemy, od&nbsp;30&nbsp;maja nie ma&nbsp;ograniczeń
         co&nbsp;do&nbsp;liczby uczestników we&nbsp;Mszy Świętej
         w&nbsp;kościele. Pamiętajmy zatem, że&nbsp;Pan Bóg
@@ -33,45 +32,48 @@
         <p>2. W&nbsp;dni powszednie Msza Święta jest odprawiana o&nbsp;6.30
         i&nbsp;18.00. Kto zamawiał intencje do&nbsp;odprawienia, niech
         sprawdza w&nbsp;gablocie na&nbsp;wywieszce. Sposobność
-        do&nbsp;spowiedzi przed Mszą Św. zarówno rano, jak
+        do&nbsp;spowiedzi przed Mszą&nbsp;Św. zarówno rano, jak
         i&nbsp;wieczorem.</p>
         <p>3. Kościół jest otwarty codziennie przez cały dzień. Zachęcamy
         do&nbsp;modlitwy przed Najświętszym Sakramentem o&nbsp;ustanie
-        epidemii i&nbsp;potrzebne łaski dla ducha i&nbsp;dla ciała.</p>
-        <p>4. Po&nbsp;wakacyjnej przerwie odprawiamy o&nbsp;12.30 Mszę
-        Świętą dla dzieci z&nbsp;rodzicami. Zapraszamy do&nbsp;licznego
-        udziału.</p>
-        <p>5. Jeśli są&nbsp;tacy Rodzice, którzy nie zapisali jeszcze swoich
+        epidemii i&nbsp;potrzebne łaski dla ducha, i&nbsp;dla ciała.</p>
+        <p>4. Jeśli są&nbsp;Rodzice, którzy nie zapisali jeszcze swoich
         dzieci do&nbsp;I–Komunii Świętej, mogą to&nbsp;uczynić dzisiaj lub
-        w&nbsp;ciągu tygodnia w&nbsp;zakrystii czy kancelarii parafialnej.</p>
-        <p>6. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, kwiaty, ofiarę
-        na&nbsp;Mszę Świętą za&nbsp;sprzątających, która będzie odprawiona
-        dzisiaj o&nbsp;godzinie 16.00, <em>Bóg zapłać</em>
-        za&nbsp;wyciągnięcie nieczystości z&nbsp;parafialnego szaletu,
-        za&nbsp;zakup krzewów, które po&nbsp;kontroli Urzędu Miejskiego mamy
-        jeszcze dosadzić w&nbsp;związku z&nbsp;wyciętym drzewem, które
-        rozwalało nam szalet. Wam <strong>Kochani</strong> bardzo dziękuję
-        za&nbsp;ofiary, które składacie na&nbsp;tacę, przesyłacie
-        na&nbsp;konto bankowe lub przekazujecie w&nbsp;zakrystii.</p>
-        <p>7. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
-        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Mickiewicza 43
-        od&nbsp;1 do&nbsp;27 – mieszkańców z&nbsp;klatki 1, 2 i&nbsp;3.</p>
-        <p>8. W&nbsp;minionym tygodniu odwołał Pan Bóg z&nbsp;naszej Parafii
-        Andrzeja Łukaszka. <em>Wieczny odpoczynek racz mu&nbsp;dać
-        Panie...</em></p>
-        <p>9. Taca w&nbsp;przyszłą niedzielę zgodnie z&nbsp;dekretem
+        w&nbsp;ciągu tygodnia w&nbsp;zakrystii czy kancelarii
+        parafialnej.</p>
+        <p>5. Legion Maryi, zaprasza jutro o&nbsp;godzinie 15.00
+        na&nbsp;koronkę do&nbsp;Bożego Miłosierdzia w&nbsp;ramach
+        ogólnoświatowej modlitwy "Koronka na&nbsp;ulicach miast świata".</p>
+        <p>6. W&nbsp;tym tygodniu wypada I&nbsp;czwartek, piątek
+        i&nbsp;sobota miesiąca października.</p>
+        <p>7. Od&nbsp;1&nbsp;października będziemy odprawiać nabożeństwo
+        różańcowe o&nbsp;godzinie 17.30.</p>
+        <p>8. W&nbsp;przyszłą niedzielę na&nbsp;Mszę Świętą o&nbsp;godzinie
+        12.30 zapraszamy wszystkie dzieci przygotowujące się do&nbsp;I-szej
+        Komunii Świętej wraz z&nbsp;Rodzicami. Poświęcimy różańce
+        i&nbsp;rodzice wręczą je&nbsp;swoim dzieciom. Różańce kupują
+        Rodzice.</p>
+        <p>9. <em>Bóg zapłać</em> za&nbsp;sprzątanie kościoła, złożoną
+        ofiarę, dary stołu i&nbsp;wszelkie dobro. Wam
+        <strong>Kochani</strong> bardzo dziękuję za&nbsp;ofiary,
+        które składacie na&nbsp;tacę, przesyłacie na&nbsp;konto
+        bankowe lub przekazujecie w&nbsp;zakrystii.</p>
+        <p>10. <strong>Do&nbsp;sprzątania prosimy w&nbsp;sobotę
+        na&nbsp;godzinę 7.00</strong> Rodziny z&nbsp;bloku Mickiewicza 43,
+        IV&nbsp;klatka, od&nbsp;28 do&nbsp;35.</p>
+        <p>11. Taca z&nbsp;dzisiejszej niedzieli, zgodnie z&nbsp;dekretem
         o&nbsp;zobowiązaniach finansowych Parafii jest przeznaczona
-        na&nbsp;remont Katedry w&nbsp;Sandomierzu.</p>
-        <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        na&nbsp;remont Katedry w&nbsp;Sandomierzu. W&nbsp;przyszłą niedzielę
+        zbiórka do&nbsp;puszek, tzw. świętopietrze. Zbiórka przeniesiona
+        przez Władze Kościelne z&nbsp;29&nbsp;czerwca.</p>
+        <p>12. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
         Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
-        <p>11. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
+        <p>13. Wszystkim, którzy w&nbsp;tym tygodniu obchodzą swoje imieniny,
         urodziny, jakiekolwiek rocznice, gościom, uczestnikom liturgii,
         składamy najlepsze życzenia i&nbsp;zapewniamy o&nbsp;naszej modlitwie
         w&nbsp;waszych intencjach.</p>
-        <p>12. Informacja Pana Burmistrza Miasta Sandomierza i&nbsp;Pana
-        Wójta Gminy Obrazów o&nbsp;spisie rolnym...</p>
         <p>
-            <span style="float: left;">Sandomierz, 20.&nbsp;września 2020&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 27.&nbsp;września 2020&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks. administrator Józef Śmigiel</span>
         </p>
     </div>
