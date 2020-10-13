7691273911d8db6e62acf97827b78847d9ac714d
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2020-10-13 19:27:13 +0100

    2020-10-13

--- parafia.php
+++ parafia.php
@@ -21,8 +21,10 @@ include 'head.php';
                     </div>
                     <ol class="list-group">
                         <li class="list-group-item">Ks. proboszcz kanonik mgr Stanisław Chmielewski (od 2020 roku)</li>
-                        <li class="list-group-item">Ks. mgr lic. Damian Dziuba (od 2017 roku)</li>
+                        <li class="list-group-item">Ks. mgr Piotr Cieśla (od 2020 roku)</li>
+                        <li class="list-group-item">Ks. mgr Krzysztof Rębisz (od 2019 roku)</li>
                         <li class="list-group-item">Ks. mgr Sebastian Mroczkowski (od 2018 roku)</li>
+                        <li class="list-group-item">Ks. mgr lic. Damian Dziuba (od 2017 roku)</li>
                     </ol>
                 </div>
                 <hr/>
